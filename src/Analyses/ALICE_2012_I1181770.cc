ed103c83f75a96bf578cc1f423fbc8c8a6034c74
Andy Buckley <andy@insectnation.org>
2012-12-07 13:21:06 +0000

    A rewrite of ALICE_2012_I1181770 -- needs to be checked by original authors to make sure logic is intact

--- src/Analyses/ALICE_2012_I1181770.cc
+++ src/Analyses/ALICE_2012_I1181770.cc
@@ -6,6 +6,7 @@
 
 namespace Rivet {
 
+
   class ALICE_2012_I1181770 : public Analysis {
   public:
 
@@ -13,160 +14,91 @@ namespace Rivet {
       : Analysis("ALICE_2012_I1181770")
     {    }
 
-  public:
 
     void init() {
-      addProjection(ChargedFinalState(),"CFS");
+      // Projection setup
+      addProjection(ChargedFinalState(), "CFS");
 
-      if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) {
-        _h_xsec_sd           = bookHistogram1D(3, 1, 1);
-        _h_xsec_dd           = bookHistogram1D(4, 1, 1);
-        _h_xsec_inel         = bookHistogram1D(5, 1, 1);
-      } else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) {
-        _h_xsec_sd           = bookHistogram1D(3, 1, 2);
-        _h_xsec_dd           = bookHistogram1D(4, 1, 2);
-        _h_xsec_inel         = bookHistogram1D(5, 1, 2);
-      } else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) {
-        _h_xsec_sd           = bookHistogram1D(3, 1, 3);
-        _h_xsec_dd           = bookHistogram1D(4, 1, 3);
-        _h_xsec_inel         = bookHistogram1D(5, 1, 3);
-      }
+      // Book (energy-specific) histograms
+      _isqrts = -1;
+      if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) _isqrts = 1;
+      else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) _isqrts = 2;
+      else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) _isqrts = 3;
+      assert(_isqrts > 0);
+      _h_xsec_sd   = bookHistogram1D(3, 1, _isqrts);
+      _h_xsec_dd   = bookHistogram1D(4, 1, _isqrts);
+      _h_xsec_inel = bookHistogram1D(5, 1, _isqrts);
     }
+
 
     void analyze(const Event& event) {
+      const ChargedFinalState& cfs = applyProjection<ChargedFinalState>(event, "CFS");
+      if (cfs.size() < 2) vetoEvent; // need at least two particles to calculate gaps
+
       const double weight = event.weight();
 
-      const ChargedFinalState& cfs = applyProjection<ChargedFinalState>(event, "CFS");
+      // Fill INEL plots for each event
+      _h_xsec_inel->fill(sqrtS()/GeV, weight);
 
-      // fill INEL plots for each event
-      if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) {
-        _h_xsec_inel->fill( 900/GeV, weight );
-      } else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) {
-        _h_xsec_inel->fill( 2760/GeV, weight );
-      } else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) {
-        _h_xsec_inel->fill( 7000/GeV, weight );
+      // Identify particles with most positive/most negative rapidities
+      const ParticleVector particlesByRap = cfs.particlesByRapidity();
+      const Particle pslowest = particlesByRap.front();
+      const Particle pfastest = particlesByRap.back();
+
+      // Find gap sizes
+      const ParticleVector particlesByEta = cfs.particlesByEta(); // sorted from minus to plus
+      const size_t num_particles = particlesByEta.size();
+      vector<double> gaps;
+      for (size_t ip = 1; ip < num_particles; ++ip) {
+        const Particle& p1 = particlesByEta[ip-1];
+        const Particle& p2 = particlesByEta[ip];
+        const double gap = p2.momentum().eta() - p1.momentum().eta();
+        assert(gap >= 0);
+        gaps.push_back(gap);
       }
 
-      double Eslowest(0.0), pslowest(0.0), yslowest(999.);
-      double Efastest(0.0), pfastest(0.0), yfastest(-999.);
-      int pidslowest(0), pidfastest(0);
-
-      double LRG(0.0), etapre(0.0), gapbwd(0.0), gapfwd(0.0);
-      unsigned int num_p(0);
-
-      FourMomentum leadP(0.,0.,0.,0.);
-      double Elead(0.0), plead(0.0);
-
-      foreach(const Particle& p, cfs.particlesByEta()) { //sorted from minus to plus
-        const PdgId pid = p.pdgId();
-        double y = p.momentum().rapidity();
-        double eta = p.momentum().eta();
-        //SD case
-        if (y < yslowest) {
-          Eslowest = p.momentum().E();
-          pslowest = p.momentum().vector3().mod();
-          yslowest = p.momentum().rapidity();
-          pidslowest = pid;
-        }
-        if (y > yfastest) {
-          Efastest = p.momentum().E();
-          pfastest = p.momentum().vector3().mod();
-          yfastest = p.momentum().rapidity();
-          pidfastest = pid;
-        }
-
-        num_p += 1;
-        // DD case
-        if (num_p==1) {
-          etapre = p.momentum().eta();
-        } else if (num_p > 1) {
-          if (num_p==2) gapbwd = fabs(eta-etapre);
-
-          double gap = fabs(eta-etapre);
-          LRG = (gap > LRG ? gap : LRG); // largest gap
-
-          if (num_p==cfs.size()) gapfwd = fabs(eta-etapre);
-          etapre = eta;
-        }
-      }
+      // First, last, and largest gaps
+      const double gapmax = *max_element(gaps.begin(), gaps.end());
+      const double gapbwd = gaps.front();
+      const double gapfwd = gaps.back();
 
       // Mx calculation
-      if (pidslowest==2212 && pidfastest==2212) {
-        if (fabs(yslowest) > fabs(yfastest)) {
-          Elead = Eslowest;
-          plead = pslowest;
-        } else if (fabs(yslowest) < fabs(yfastest)) {
-          Elead = Efastest;
-          plead = pfastest;
-        } else {
-          Elead = Eslowest;
-          plead = pslowest;
-          if ( (double)rand() / (double)RAND_MAX > 0.5) { // generate random number in [0.,1.] range and make decision randomly
-            Elead = Efastest;
-            plead = pfastest;
-          }
-        }
-      } else if (pidslowest==2212) {
-        Elead = Eslowest;
-        plead = pslowest;
-      } else if (pidfastest==2212) {
-        Elead = Efastest;
-        plead = pfastest;
+      FourMomentum p4lead;
+      if (pslowest.pdgId() == PROTON && pfastest.pdgId() == PROTON) {
+        p4lead = (fabs(pslowest.momentum().rapidity()) > fabs(pfastest.momentum().rapidity())) ? pslowest.momentum() : pfastest.momentum();
+      } else if (pslowest.pdgId() == PROTON) {
+        p4lead = pslowest.momentum();
+      } else if (pfastest.pdgId() == PROTON) {
+        p4lead = pfastest.momentum();
+      }
+      const double Mx = sqrt( (sqrtS()-p4lead.E()-p4lead.vector3().mod()) * (sqrtS()-p4lead.E()+p4lead.vector3().mod()) );
+
+      // Fill SD (and escape) if Mx is sufficiently low
+      if (Mx < 200*GeV) {
+        _h_xsec_sd->fill(sqrtS()/GeV, weight);
+        return;
       }
 
-      double Mx = sqrt((sqrtS()/GeV-Elead-plead)*(sqrtS()/GeV-Elead+plead));
-      bool singleDiff = false;
-
-      // Fill SD
-      if (Mx < 200.) {
-        singleDiff = true;
-        if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) {
-          _h_xsec_sd->fill( 900/GeV, weight);
-        } else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) {
-          _h_xsec_sd->fill( 2760/GeV, weight);
-        } else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) {
-          _h_xsec_sd->fill( 7000/GeV, weight);
-        }
-      }
-
-      if ( singleDiff ) vetoEvent; // DD events are defined as NSD with large gap.
-
-      // also remove SD-like events in NSD events
-      if ( std::abs(gapbwd-LRG) < std::numeric_limits<double>::epsilon() || std::abs(gapfwd-LRG) < std::numeric_limits<double>::epsilon() ) vetoEvent;
+      // Also remove SD-like events in NSD events
+      if (fuzzyEquals(gapbwd, gapmax) || fuzzyEquals(gapfwd, gapmax)) vetoEvent;
 
       // Fill DD plots
-      if (LRG > 3.) {
-        if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) {
-          _h_xsec_dd->fill( 900/GeV, weight);
-        } else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) {
-          _h_xsec_dd->fill( 2760/GeV, weight);
-        } else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) {
-          _h_xsec_dd->fill( 7000/GeV, weight);
-        }
-      }
+      if (gapmax > 3) _h_xsec_dd->fill(sqrtS()/GeV, weight);
     }
+
 
     void finalize() {
-
-      // get the ratio plots: SD/inel, DD/inel
+      // Make the ratio plots: SD/inel, DD/inel
       const string dir = histoDir();
+      const string ypart = "y0" + lexical_cast<string>(_isqrts);
+      histogramFactory().divide( dir + "/d01-x01-" + ypart, *_h_xsec_sd, *_h_xsec_inel);
+      histogramFactory().divide( dir + "/d02-x01-" + ypart, *_h_xsec_dd, *_h_xsec_inel);
 
-      if (fuzzyEquals(sqrtS()/GeV, 900, 1E-3)) {
-        histogramFactory().divide( dir + "/d01-x01-y01", *_h_xsec_sd , *_h_xsec_inel );
-        histogramFactory().divide( dir + "/d02-x01-y01", *_h_xsec_dd , *_h_xsec_inel );
-      } else if (fuzzyEquals(sqrtS()/GeV, 2760, 1E-3)) {
-        histogramFactory().divide( dir + "/d01-x01-y02", *_h_xsec_sd , *_h_xsec_inel );
-        histogramFactory().divide( dir + "/d02-x01-y02", *_h_xsec_dd , *_h_xsec_inel );
-      } else if (fuzzyEquals(sqrtS()/GeV, 7000, 1E-3)) {
-        histogramFactory().divide( dir + "/d01-x01-y03", *_h_xsec_sd , *_h_xsec_inel );
-        histogramFactory().divide( dir + "/d02-x01-y03", *_h_xsec_dd , *_h_xsec_inel );
-      }
-
-      scale(_h_xsec_sd,   crossSection()/millibarn/sumOfWeights());
-      scale(_h_xsec_dd,   crossSection()/millibarn/sumOfWeights());
+      scale(_h_xsec_sd, crossSection()/millibarn/sumOfWeights());
+      scale(_h_xsec_dd, crossSection()/millibarn/sumOfWeights());
       scale(_h_xsec_inel, crossSection()/millibarn/sumOfWeights());
-
     }
+
 
   private:
 
@@ -174,9 +106,12 @@ namespace Rivet {
     AIDA::IHistogram1D *_h_xsec_dd;
     AIDA::IHistogram1D *_h_xsec_inel;
 
+    /// Energy index for histogram IDs
+    int _isqrts;
+
   };
 
-  // Hook for the plugin system
+
   DECLARE_RIVET_PLUGIN(ALICE_2012_I1181770);
 
 }
