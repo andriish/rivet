ed5d5f640448355e7619d6bb716560aefb4450c0
Peter Richardson <Peter.Richardson@durham.ac.uk>
2019-11-10 14:20:49 +0000

    add missing header

--- analyses/pluginSLAC/TPC_1988_I262143.cc
+++ analyses/pluginSLAC/TPC_1988_I262143.cc
@@ -1,6 +1,7 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/Beam.hh"
 #include "Rivet/Projections/FastJets.hh"
 
 namespace Rivet {
