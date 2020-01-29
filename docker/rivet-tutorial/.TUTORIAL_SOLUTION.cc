60b3b49e6cb9d41180430c7ff709c964de84c234
Andy Buckley <a.g.buckley@gmail.com>
2020-01-29 17:51:23 +0000

    ATLAS tutorial tweaks

--- docker/rivet-tutorial/.TUTORIAL_SOLUTION.cc
+++ docker/rivet-tutorial/.TUTORIAL_SOLUTION.cc
@@ -22,7 +22,12 @@ namespace Rivet {
       _eemode = (getOption("LMODE") != "MU");
 
       // Projections
-      //...
+      FinalState leps(Cuts::pT > 20*GeV && Cuts::abspid == (_eemode ? PID::ELECTRON : PID::MUON));
+      declare("Leps", leps);
+      DressedLeptons dleps(0.1, Cuts::pT > 20*GeV && Cuts::abspid == (_eemode ? PID::ELECTRON : PID::MUON));
+      declare("DLeps", dleps);
+      FastJets jets(FinalState(), FastJets::ANTIKT, 0.4);
+      declare("Jets", jets);
 
       // Book histograms
       vector<double> mll_bins = { 66., 74., 78., 82., 84., 86., 88., 89., 90., 91.,
@@ -49,19 +54,26 @@ namespace Rivet {
       // 5. Count the number of b-jets, using the ATLAS pT_B > 5 GeV tag definition
 
       // Get leptons (bare & dressed)
-      //...
+      const Particles leps = apply<FinalState>(event, "Leps").particlesByPt();
+      const Particles dleps = apply<FinalState>(event, "DLeps").particlesByPt();
 
       // Get jets and remove overlaps
-      //...
+      const Jets alljets = apply<JetFinder>(event, "Jets").jetsByPt(Cuts::pT > 20*GeV && Cuts::absrap < 4.5);
+      const Jets isojets = discardIfAnyDeltaRLess(alljets, dleps, 0.4);
 
       // m_ll
-      //...
+      if (leps.size() == 2) _h["mll_bare"]->fill( (leps[0].mom() + leps[1].mom()).mass()/GeV );
+      if (dleps.size() == 2) _h["mll_dressed"]->fill( (dleps[0].mom() + dleps[1].mom()).mass()/GeV );
 
       // Jets
-      //...
+      _h["njet_all"]->fill(alljets.size());
+      _h["njet_iso"]->fill(isojets.size());
+      _h["jet1pt"]->fill(isojets[0].pT()/GeV);
+      _h["HT"]->fill(sum(isojets, pT, 0.0)/GeV + sum(head(dleps, 2), pT, 0.0)/GeV);
 
       // b-jets
-      //...
+      _h["nbjet_all"]->fill(select(alljets, hasBTag(Cuts::pT > 5*GeV)).size());
+      _h["nbjet_iso"]->fill(select(isojets, hasBTag(Cuts::pT > 5*GeV)).size());
 
     }
 
--- docker/rivet-tutorial/TUTORIAL.plot
+++ docker/rivet-tutorial/TUTORIAL.plot
@@ -2,7 +2,7 @@ BEGIN PLOT /TUTORIAL/.*
 LegendAlign=r
 END PLOT
 
-BEGIN PLOT /TUTORIAL/mass_*
+BEGIN PLOT /TUTORIAL/mll_*
 Title=Dilepton invariant mass spectrum
 XLabel=$m_{\ell\ell}$ [GeV]
 YLabel=$\text{d}\sigma / \text{d} m_{\ell\ell}$ [pb / GeV]
@@ -11,14 +11,14 @@ LegendXPos=0.05
 LegendYPos=0.20
 END PLOT
 
-BEGIN PLOT /TUTORIAL/jets_*
+BEGIN PLOT /TUTORIAL/njet_*
 Title=Exclusive jet multiplicity
 XMinorTickMarks=0
 XLabel=$N_\text{jets}$
 YLabel=$\text{d}\sigma / \text{d} N_\text{jets}$ [pb]
 END PLOT
 
-BEGIN PLOT /TUTORIAL/bjets_*
+BEGIN PLOT /TUTORIAL/nbjet_*
 Title=Exclusive $b$-jet multiplicity
 XCustomMajorTicks=0	0	1	1	2	2
 XLabel=$N_b$
