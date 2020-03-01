33ff74708f9f67c11a0b72a4b92140f0ee9b599c
Peter Richardson <peter.richardson@durham.ac.uk>
2020-03-01 16:56:25 +0100

    Add ARGUS_1993_I357132 Lambda_c* spectrum

--- analyses/pluginDORIS/ARGUS_1993_I357132.cc
+++ analyses/pluginDORIS/ARGUS_1993_I357132.cc
@@ -6,12 +6,12 @@
 namespace Rivet {
 
 
-  /// @brief Excited Lambda_c spectra
-  class CLEOII_1994_I381696 : public Analysis {
+  /// @brief Add a short analysis description here
+  class ARGUS_1993_I357132 : public Analysis {
   public:
 
     /// Constructor
-    DEFAULT_RIVET_ANALYSIS_CTOR(CLEOII_1994_I381696);
+    DEFAULT_RIVET_ANALYSIS_CTOR(ARGUS_1993_I357132);
 
 
     /// @name Analysis methods
@@ -23,40 +23,28 @@ namespace Rivet {
       declare(Beam(), "Beams");
       declare(UnstableParticles(), "UFS");
       // book histos
-      book(_h_2595,5,1,1);
-      book(_h_2625,6,1,1);
+      book(_h_x,1,1,1);
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
-      static const int id2595 = 14122;
       static const int id2625 = 4124;
       // Get beams and average beam momentum
       const ParticlePair& beams = apply<Beam>(event, "Beams").beams();
       const double Emax = ( beams.first.p3().mod() + beams.second.p3().mod() ) / 2.0;
+      const double Pmax = sqrt(sqr(Emax)-sqr(2.625));
       const UnstableParticles& ufs = apply<UnstableFinalState>(event, "UFS");
-      for (const Particle& p : ufs.particles(Cuts::abspid==id2595 or
-					     Cuts::abspid==id2625)) {
-	// spectrum
-	if(p.abspid()==id2595) {
-	  double Pmax = sqrt(sqr(Emax)-sqr(2.595));
-	  double xp = p.momentum().p3().mod()/Pmax;
-	  _h_2595->fill(xp);
-	}
-	else {
-	  double Pmax = sqrt(sqr(Emax)-sqr(2.625));
-	  double xp = p.momentum().p3().mod()/Pmax;
-	  _h_2625->fill(xp);
-	}
+      for (const Particle& p : ufs.particles(Cuts::abspid==id2625)) {
+	double xp = p.momentum().p3().mod()/Pmax;
+	_h_x->fill(xp);
       }
     }
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-      normalize(_h_2595);
-      normalize(_h_2625);
+      normalize(_h_x);
     }
 
     ///@}
@@ -64,13 +52,13 @@ namespace Rivet {
 
     /// @name Histograms
     ///@{
-    Histo1DPtr _h_2595,_h_2625;
+    Histo1DPtr _h_x;
     ///@}
 
 
   };
 
 
-  DECLARE_RIVET_PLUGIN(CLEOII_1994_I381696);
+  DECLARE_RIVET_PLUGIN(ARGUS_1993_I357132);
 
 }
