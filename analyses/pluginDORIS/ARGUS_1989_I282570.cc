037487ca5ef46dec0bf030f56cdce874707a6c9b
Peter Richardson <peter.richardson@durham.ac.uk>
2020-03-02 13:17:09 +0100

    Add ARGUS_1989_I282570 D_s1(2536) spectrum

---
+++ analyses/pluginDORIS/ARGUS_1989_I282570.cc
@@ -0,0 +1,64 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/Beam.hh"
+#include "Rivet/Projections/UnstableParticles.hh"
+
+namespace Rivet {
+
+
+  /// @brief Spectrum for D_s1 
+  class ARGUS_1989_I282570 : public Analysis {
+  public:
+
+    /// Constructor
+    DEFAULT_RIVET_ANALYSIS_CTOR(ARGUS_1989_I282570);
+
+
+    /// @name Analysis methods
+    ///@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+      // projections
+      declare(Beam(), "Beams");
+      declare(UnstableParticles(), "UFS");
+      // book histos
+      book(_h_x     ,2,1,1);
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+      static const int DsID = 10433;
+      // Get beams and average beam momentum
+      const ParticlePair& beams = apply<Beam>(event, "Beams").beams();
+      const double Emax = ( beams.first.p3().mod() + beams.second.p3().mod() ) / 2.0;
+      const double Pmax = sqrt(sqr(Emax)-sqr(2.535));
+      const UnstableParticles& ufs = apply<UnstableFinalState>(event, "UFS");
+      for (const Particle& p : ufs.particles(Cuts::abspid==DsID)) {
+	double xp = p.momentum().p3().mod()/Pmax;
+        _h_x->fill(xp);
+      }
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+      normalize(_h_x     );
+    }
+
+    ///@}
+
+
+    /// @name Histograms
+    ///@{
+    Histo1DPtr _h_x;
+    ///@}
+
+
+  };
+
+
+  DECLARE_RIVET_PLUGIN(ARGUS_1989_I282570);
+
+}
---
+++ analyses/pluginDORIS/ARGUS_1989_I282570.plot
@@ -0,0 +1,6 @@
+BEGIN PLOT /ARGUS_1989_I282570/d02-x01-y01
+Title=Spectrum for $D_{s1}(2536)^+$ production
+XLabel=$x_p$
+YLabel=$1/N\text{d}N/\text{d}x_p$
+LogY=0
+END PLOT
