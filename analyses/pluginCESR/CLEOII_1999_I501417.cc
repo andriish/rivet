d356c27da7e5a725e01d606fe89c7682cbe604bd
Peter Richardson <peter.richardson@durham.ac.uk>
2020-03-03 11:25:15 +0100

    Add CLEOII_1999_I501417 Xi_c1 spectra

--- analyses/pluginCESR/CLEOII_1999_I501417.cc
+++ analyses/pluginCESR/CLEOII_1999_I501417.cc
@@ -6,12 +6,12 @@
 namespace Rivet {
 
 
-  /// @brief Xi_c' spectrum
-  class CLEOII_1999_I478217 : public Analysis {
+  /// @brief Xi_c1 spectra
+  class CLEOII_1999_I501417 : public Analysis {
   public:
 
     /// Constructor
-    DEFAULT_RIVET_ANALYSIS_CTOR(CLEOII_1999_I478217);
+    DEFAULT_RIVET_ANALYSIS_CTOR(CLEOII_1999_I501417);
 
 
     /// @name Analysis methods
@@ -23,18 +23,19 @@ namespace Rivet {
       declare(Beam(), "Beams");
       declare(UnstableParticles(), "UFS");
       // book histos
-      book(_h_Xi_c,1,1,1);
+      book(_h_Xi_c,2,1,1);
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
+      static const int id0 = 14314, idp=14324;
       // Get beams and average beam momentum
       const ParticlePair& beams = apply<Beam>(event, "Beams").beams();
       const double Emax = ( beams.first.p3().mod() + beams.second.p3().mod() ) / 2.0;
-      const double Pmax = sqrt(sqr(Emax)-sqr(2.578));
+      const double Pmax = sqrt(sqr(Emax)-sqr(2.815));
       const UnstableParticles& ufs = apply<UnstableFinalState>(event, "UFS");
-      for (const Particle& p : ufs.particles(Cuts::abspid==4312 or Cuts::abspid==4322)) {
+      for (const Particle& p : ufs.particles(Cuts::abspid==id0 or Cuts::abspid==idp)) {
 	double xp = p.momentum().p3().mod()/Pmax;
         _h_Xi_c->fill(xp);
       }
@@ -58,6 +59,6 @@ namespace Rivet {
   };
 
 
-  DECLARE_RIVET_PLUGIN(CLEOII_1999_I478217);
+  DECLARE_RIVET_PLUGIN(CLEOII_1999_I501417);
 
 }
