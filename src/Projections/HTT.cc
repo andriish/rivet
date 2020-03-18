bc54ecf2cd93c60937bea72ae916d30b18ab9a08
jackaraz <jackaraz@gmail.com>
2020-03-18 12:54:58 +0000

    Test tagger as accessible variable
    instead of running and defining tagger in a for loop, defining first run later

--- src/Projections/HTT.cc
+++ src/Projections/HTT.cc
@@ -10,6 +10,20 @@ HTT::HTT(const JetAlg& jetalg)
 {
     setName("HEPTopTagger");
     declare(jetalg, "Jets");
+  _tagger.set_max_subjet_mass(30.);
+  _tagger.set_mass_drop_threshold(0.8);
+  _tagger.set_filtering_R(0.3);
+  _tagger.set_filtering_n(5);
+  _tagger.set_filtering_minpt_subjet(30.);
+
+  // How to select among candidates
+  _tagger.set_mode(HEPTopTagger::TWO_STEP_FILTER);
+
+  // Requirements to accept a candidate
+  _tagger.set_top_minpt(200);
+  _tagger.set_top_mass_range(150., 200.);
+  _tagger.set_fw(0.15);
+  _tagger.set_debug(1);
 }
 
 CmpState HTT::compare(const Projection& p) const 
@@ -30,28 +44,30 @@ void HTT::calc(const Jets& jets) {
 //    for (const Jet& jet : jets) {
     for (unsigned i=0; i<jets.size();i++) {
       // Apply jet cuts
-      HEPTopTagger::HEPTopTagger tagger(jets[i]);
+//      HEPTopTagger::HEPTopTagger tagger(jets[i]);
+        _tagger(jets[i])
              // Unclustering, Filtering & Subjet Settings
-      tagger.set_max_subjet_mass(30.);
-      tagger.set_mass_drop_threshold(0.8);
-      tagger.set_filtering_R(0.3);
-      tagger.set_filtering_n(5);
-      tagger.set_filtering_minpt_subjet(30.);
-
-      // How to select among candidates
-      tagger.set_mode(HEPTopTagger::TWO_STEP_FILTER);
-
-      // Requirements to accept a candidate
-      tagger.set_top_minpt(200);
-      tagger.set_top_mass_range(150., 200.);
-      tagger.set_fw(0.15);
+//      tagger.set_max_subjet_mass(30.);
+//      tagger.set_mass_drop_threshold(0.8);
+//      tagger.set_filtering_R(0.3);
+//      tagger.set_filtering_n(5);
+//      tagger.set_filtering_minpt_subjet(30.);
+//
+//      // How to select among candidates
+//      tagger.set_mode(HEPTopTagger::TWO_STEP_FILTER);
+//
+//      // Requirements to accept a candidate
+//      tagger.set_top_minpt(200);
+//      tagger.set_top_mass_range(150., 200.);
+//      tagger.set_fw(0.15);
 
       // Run the tagger
-      tagger.run();
+      _tagger.run();
+      MSG_INFO("Maybe top: " << _tagger.is_maybe_top());
             // Look at output if we have a tag:
       if (tagger.is_tagged()){
         MSG_INFO("Input fatjet: " << i << "  pT = " << jets[i].perp());
-        MSG_INFO("Output: pT = " << tagger.t().perp() << " Mass = " << tagger.t().m() << " f_rec = " << tagger.f_rec());
+        MSG_INFO("Output: pT = " << _tagger.t().perp() << " Mass = " << _tagger.t().m() << " f_rec = " << _tagger.f_rec());
       } else {
           MSG_INFO("Not tagged");
       }
