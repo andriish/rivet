8f7fb6669c9677f677e44308d29c209dc2420013
Christian Gutschow <chris.g@cern.ch>
2020-04-28 21:43:36 +0100

    add HD sync patch for BELLE_2009_I823878

--- pyext/rivet/hepdatapatches/__init__.py
+++ pyext/rivet/hepdatapatches/__init__.py
@@ -3,4 +3,5 @@ from . import ATLAS_2019_I1734263
 from . import ATLAS_2019_I1746286
 from . import BABAR_2001_I558091
 from . import BABAR_2006_I731865
+from . import BELLE_2009_I823878
 from . import CMS_2016_I1487288
