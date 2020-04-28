6856d3065f8ee5bade8faec713808053a0a84258
Christian Gutschow <chris.g@cern.ch>
2020-04-28 21:53:06 +0100

    add HD sync patch for CLEO_1984_I193577

--- pyext/rivet/hepdatapatches/__init__.py
+++ pyext/rivet/hepdatapatches/__init__.py
@@ -4,4 +4,5 @@ from . import ATLAS_2019_I1746286
 from . import BABAR_2001_I558091
 from . import BABAR_2006_I731865
 from . import BELLE_2009_I823878
+from . import CLEO_1984_I193577
 from . import CMS_2016_I1487288
