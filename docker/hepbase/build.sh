2972b81061941745690921d1321eb0d322c6f0a2
Andy Buckley <andy.buckley@cern.ch>
2020-05-31 16:58:35 +0100

    Builds now mostly working, and add a dedicated single Py2 build

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -2,24 +2,24 @@
 
 set -e
 
-VPYTHONS="2" # 3"
-VHEPMCS="2" # 3"
+for vhepmc in 2 3; do
+    MSG="Building hepbase image with HepMC=$vhepmc"
+    BASEARGS="--build-arg HEPMC_VERSION=$vhepmc"
+    GCCARGS="$BASEARGS --build-arg CXX_CMD=g++ --build-arg CC_CMD=gcc --build-arg FC=gfortran"
+    CLANGARGS="$BASEARGS --build-arg CXX_CMD=clang++ --build-arg CC_CMD=clang --build-arg FC_CMD=gfortran"
 
-for vpython in $VPYTHONS; do
-    for vhepmc in $VHEPMCS; do
-        MSG="Building hepbase image with Python=$vpython, HepMC=$vhepmc"
-        BASEARGS="--build-arg PYTHON_VERSION=$vpython --build-arg HEPMC_VERSION=$vhepmc"
+    echo "@@ $MSG on Debian with GCC compilers and Python 2"
+    docker build . -f Dockerfile.debpy2 $GCCARGS -t hepstore/debian-gcc-hepmc${vhepmc}-py2
 
-        # GCCARGS="$BASEARGS --build-arg CXX_CMD=g++ --build-arg CC_CMD=gcc --build-arg FC=gfortran"
-        # echo "@@ $MSG on Ubuntu with GCC compilers"
-        # docker build . -f Dockerfile.ubuntu $GCCARGS -t hepstore/ubuntu-gcc-py${vpython}-hepmc${vhepmc}
-        # echo "@@ $MSG on Fedora with GCC compilers"
-        # docker build . -f Dockerfile.fedora $GCCARGS -t hepstore/fedora-gcc-py${vpython}-hepmc${vhepmc}
+    echo "@@ $MSG on Ubuntu with GCC compilers"
+    docker build . -f Dockerfile.ubuntu $GCCARGS -t hepstore/ubuntu-gcc-hepmc${vhepmc}-py3
+    echo "@@ $MSG on Fedora with GCC compilers"
+    docker build . -f Dockerfile.fedora $GCCARGS -t hepstore/fedora-gcc-hepmc${vhepmc}-py3
 
-        CLANGARGS="$BASEARGS --build-arg CXX_CMD=clang++ --build-arg CC_CMD=clang --build-arg FC=gfortran"
-        # echo "@@ $MSG on Ubuntu with clang compilers"
-        # docker build . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/ubuntu-clang-py${vpython}-hepmc${vhepmc}
-        echo "@@ $MSG on Fedora with clang/LLVM compilers"
-        docker build . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/fedora-clang-py${vpython}-hepmc${vhepmc}
-    done
+    echo "@@ $MSG on Ubuntu with clang compilers"
+    docker build . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/ubuntu-clang-hepmc${vhepmc}-py3
+    # echo "@@ $MSG on Fedora with clang/LLVM compilers"
+    # docker build . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/fedora-clang-hepmc${vhepmc}-py3
+
+    # TODO: Add Python2 image (GCC only)
 done
