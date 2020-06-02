1436fdfca370e146284f8ffb102dc485a0b79523
Andy Buckley <andy.buckley@cern.ch>
2020-06-02 10:48:28 +0100

    Dockerfile tweaks

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -5,21 +5,21 @@ set -e
 for vhepmc in 2 3; do
     MSG="Building hepbase image with HepMC=$vhepmc"
     BASEARGS="--build-arg HEPMC_VERSION=$vhepmc"
-    GCCARGS="$BASEARGS --build-arg CXX_CMD=g++ --build-arg CC_CMD=gcc --build-arg FC=gfortran"
+    GCCARGS="$BASEARGS --build-arg CXX_CMD=g++ --build-arg CC_CMD=gcc --build-arg FC_CMD=gfortran"
     CLANGARGS="$BASEARGS --build-arg CXX_CMD=clang++ --build-arg CC_CMD=clang --build-arg FC_CMD=gfortran"
 
-    echo "@@ $MSG on Debian with GCC compilers and Python 2"
-    docker build . -f Dockerfile.debpy2 $GCCARGS -t hepstore/debian-gcc-hepmc${vhepmc}-py2
-
     echo "@@ $MSG on Ubuntu with GCC compilers"
-    docker build . -f Dockerfile.ubuntu $GCCARGS -t hepstore/ubuntu-gcc-hepmc${vhepmc}-py3
+    docker build . -f Dockerfile.ubuntu $GCCARGS -t hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py3
+
     echo "@@ $MSG on Fedora with GCC compilers"
-    docker build . -f Dockerfile.fedora $GCCARGS -t hepstore/fedora-gcc-hepmc${vhepmc}-py3
+    docker build . -f Dockerfile.fedora $GCCARGS -t hepstore/hepbase-fedora-gcc-hepmc${vhepmc}-py3
 
     echo "@@ $MSG on Ubuntu with clang compilers"
-    docker build . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/ubuntu-clang-hepmc${vhepmc}-py3
-    # echo "@@ $MSG on Fedora with clang/LLVM compilers"
-    # docker build . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/fedora-clang-hepmc${vhepmc}-py3
+    docker build . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/hepbase-ubuntu-clang-hepmc${vhepmc}-py3
 
-    # TODO: Add Python2 image (GCC only)
+    # echo "@@ $MSG on Fedora with clang/LLVM compilers"
+    # docker build . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/hepbase-fedora-clang-hepmc${vhepmc}-py3
+
+    echo "@@ $MSG on Debian with GCC compilers and Python 2"
+    docker build . -f Dockerfile.debpy2 $GCCARGS -t hepstore/hepbase-debian-gcc-hepmc${vhepmc}-py2
 done
