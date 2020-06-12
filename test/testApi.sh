36852bb8f722cd8dccc061c1a33dbcb9f91a9b73
Andy Buckley <andy.buckley@cern.ch>
2020-06-12 21:34:40 +0100

    Remove attempt at out-of-source compatibility

--- test/testApi.sh
+++ test/testApi.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
-bash $top_builddir/bin/rivet-build $top_srcdir/analyses/pluginMC/EXAMPLE.cc $top_srcdir/analyses/pluginMC/MC_JETS.cc $top_srcdir/analyses/pluginMC/EXAMPLE_CUTS.cc $top_srcdir/analyses/pluginMC/EXAMPLE_SMEAR.cc
+# TODO: out-of-source compatibility
+# bash $top_builddir/bin/rivet-build $top_srcdir/analyses/pluginMC/EXAMPLE.cc $top_srcdir/analyses/pluginMC/MC_JETS.cc $top_srcdir/analyses/pluginMC/EXAMPLE_CUTS.cc $top_srcdir/analyses/pluginMC/EXAMPLE_SMEAR.cc
+bash ../bin/rivet-build ../analyses/pluginMC/EXAMPLE.cc ../analyses/pluginMC/MC_JETS.cc ../analyses/pluginMC/EXAMPLE_CUTS.cc ../analyses/pluginMC/EXAMPLE_SMEAR.cc
 export RIVET_ANALYSIS_PATH=$RIVET_ANALYSIS_PATH:$PWD
 
 exec ./testApi "$srcdir/testApi.hepmc"
