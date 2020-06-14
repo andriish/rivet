3ab672c4d08980a45a08e73ce7dee39d8d21b044
Neil Warrack <neil.warrack@gmail.com>
2020-06-14 09:07:42 +0000

    add ATLAS_2020_I1790439

--- pyext/rivet/hepdatapatches/__init__.py
+++ pyext/rivet/hepdatapatches/__init__.py
@@ -2,6 +2,7 @@ from . import ATLAS_2016_I1468168
 from . import ATLAS_2018_I1635273
 from . import ATLAS_2019_I1734263
 from . import ATLAS_2019_I1746286
+from . import ATLAS_2020_I1790439
 from . import BABAR_2001_I558091
 from . import BABAR_2006_I731865
 from . import BELLE_2009_I823878
