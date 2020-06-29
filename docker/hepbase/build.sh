843258501fc68626e645f3ecd4eb87f2d03867bc
Andy Buckley <andy.buckley@cern.ch>
2020-06-29 20:56:16 +0100

    Disable the dvilualatex plotting backend for now, as it produces errors on several systems

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+BUILD="docker build" # --squash"
 for vhepmc in 2 3; do
     for tex in 0 1; do
 
@@ -13,7 +14,7 @@ for vhepmc in 2 3; do
         test "$tex" = 1 && TEXSUFFIX="-latex"
 
         echo "@@ $MSG on Ubuntu with GCC compilers"
-        docker build . -f Dockerfile.ubuntu $GCCARGS -t hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu $GCCARGS -t hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
         echo -e "\n\n\n"
 
         echo "@@ $MSG on Fedora with GCC compilers"
@@ -23,15 +24,15 @@ for vhepmc in 2 3; do
         echo -e "\n\n\n"
 
         echo "@@ $MSG on Ubuntu with clang compilers"
-        docker build . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/hepbase-ubuntu-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/hepbase-ubuntu-clang-hepmc${vhepmc}-py3$TEXSUFFIX
         echo -e "\n\n\n"
 
         # echo "@@ $MSG on Fedora with clang/LLVM compilers"
-        # docker build . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/hepbase-fedora-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        # $BUILD . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/hepbase-fedora-clang-hepmc${vhepmc}-py3$TEXSUFFIX
         # echo -e "\n\n\n"
 
         echo "@@ $MSG on Debian with GCC compilers and Python 2"
-        docker build . -f Dockerfile.debpy2 $GCCARGS -t hepstore/hepbase-debian-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
+        $BUILD . -f Dockerfile.debpy2 $GCCARGS -t hepstore/hepbase-debian-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
         echo -e "\n\n\n"
     done
 done
