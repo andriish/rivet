d97e435bd09a5f1e415cf00faacb988fe430f9e1
Andy Buckley <andy.buckley@cern.ch>
2020-07-22 23:24:01 +0100

    Move pushing functionality into hepbase build.sh, and fix up rivet Dockerfile to handle a HepMC3 base image

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -6,8 +6,8 @@ BUILD="docker build" # --squash"
 test "$TEST" && BUILD="echo $BUILD"
 
 for vhepmc in 2 3; do
-    # for tex in 0 1; do
-    for tex in 0; do
+    for tex in 0 1; do
+    # for tex in 0; do
 
         MSG="Building hepbase image with HepMC=$vhepmc and TeX=$tex"
 
@@ -17,27 +17,39 @@ for vhepmc in 2 3; do
         if [[ "$tex" = 1 ]]; then TEXSUFFIX="-latex"; else TEXSUFFIX=""; fi
 
         echo "@@ $MSG on Ubuntu with GCC compilers"
-        $BUILD . -f Dockerfile.ubuntu $GCCARGS -t hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
+        tag=hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu $GCCARGS -t $tag
+        test "$PUSH" = 1 && docker push $tag
         echo -e "\n\n\n"
 
         echo "@@ $MSG on Fedora with GCC compilers"
-        $BUILD . -f Dockerfile.fedora $GCCARGS -t hepstore/hepbase-fedora-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
+        tag=hepstore/hepbase-fedora-gcc-hepmc${vhepmc}-py3$TEXSUFFIX
+        $BUILD . -f Dockerfile.fedora $GCCARGS -t $tag
+        test "$PUSH" = 1 && docker push $tag
         echo -e "\n\n\n"
 
         echo "@@ $MSG on Ubuntu with clang compilers"
-        $BUILD . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t hepstore/hepbase-ubuntu-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        tag=hepstore/hepbase-ubuntu-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu ${CLANGARGS/gfortran/flang} -t $tag
+        test "$PUSH" = 1 && docker push $tag
         echo -e "\n\n\n"
 
         # echo "@@ $MSG on Fedora with clang/LLVM compilers"
-        # $BUILD . -f Dockerfile.fedora ${CLANGARGS}                -t hepstore/hepbase-fedora-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        # tag=hepstore/hepbase-fedora-clang-hepmc${vhepmc}-py3$TEXSUFFIX
+        # $BUILD . -f Dockerfile.fedora ${CLANGARGS} -t $tag
+        # test "$PUSH" = 1 && docker push $tag
         # echo -e "\n\n\n"
 
         echo "@@ $MSG on Ubuntu with GCC compilers and Python 2"
-        $BUILD . -f Dockerfile.ubuntu-py2 $GCCARGS -t hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
+        tag=hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu-py2 $GCCARGS -t $tag
+        test "$PUSH" = 1 && docker push $tag
         echo -e "\n\n\n"
 
         # echo "@@ $MSG on Debian with GCC compilers and Python 2"
-        # $BUILD . -f Dockerfile.debpy2 $GCCARGS -t hepstore/hepbase-debian-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
+        # tag=hepstore/hepbase-debian-gcc-hepmc${vhepmc}-py2$TEXSUFFIX
+        # $BUILD . -f Dockerfile.debpy2 $GCCARGS -t $tag
+        # test "$PUSH" = 1 && docker push $tag
         # echo -e "\n\n\n"
 
     done
--- docker/hepbase/push.sh
+++
@@ -1,7 +0,0 @@
-for x in {ubuntu,fedora,debian}-{gcc,clang}-hepmc{2,3}-py{2,3}; do
-    for t in "" "-latex"; do
-        echo $x$t
-        docker push hepstore/hepbase-$x$t
-        echo
-    done
-done
