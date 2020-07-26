94038203a033d09af599379e59cecf30121b50c1
Andy Buckley <andy.buckley@cern.ch>
2020-07-26 22:08:14 +0100

    Update Docker scripts again

--- docker/rivet/build.sh
+++ docker/rivet/build.sh
@@ -7,14 +7,18 @@ BUILD="docker build . -f Dockerfile --build-arg RIVET_VERSION=$VERSION" # --squa
 test "$TEST" && BUILD="echo $BUILD"
 
 MSG="Building Rivet $VERSION image with architecture ="
-for vhepmc in 2 3; do
+for vhepmc in 3 2; do
     for arch in ubuntu-gcc-hepmc$vhepmc-py3 ubuntu-gcc-hepmc$vhepmc-py3 ubuntu-gcc-hepmc$vhepmc-py2 ubuntu-clang-hepmc$vhepmc-py3; do
 
         echo "@@ $MSG $arch"
-        tag="rivet:$VERSION-$arch"
-        $BUILD --build-arg BASE=$arch -t $tag
-        test "$PUSH" = 1 && docker push $tag
+        tag="hepstore/rivet:$VERSION-$arch"
+        $BUILD --build-arg ARCH=$arch -t $tag
+        # test "$PUSH" = 1 && docker push $tag
         echo -e "\n\n\n"
 
     done
 done
+
+docker tag hepstore/rivet:$VERSION{-ubuntu-gcc-hepmc2-py3,}
+docker tag hepstore/rivet:$VERSION{-ubuntu-gcc-hepmc3-py3,-hepmc3}
+docker tag hepstore/rivet:{$VERSION,latest}
