5b0e1f83e44ddaee8e5330860e65a7806c7c5bbc
Andy Buckley <andy.buckley@cern.ch>
2020-10-14 14:32:47 +0100

    Sync hepbase py2/py3 Dockerfiles, and add equivalent py2/py3 variations for the independent rivet-herwig image

--- docker/rivet-herwig/build.sh
+++ docker/rivet-herwig/build.sh
@@ -7,19 +7,26 @@ YODA_VERSION=1.8.3
 THEPEG_VERSION=2.2.1
 HERWIG_VERSION=7.2.1
 
-BUILD="docker build . -f Dockerfile"
+BUILD="docker build ."
 BUILD="$BUILD --build-arg RIVET_VERSION=${RIVET_VERSION} --build-arg YODA_VERSION=${YODA_VERSION}"
 BUILD="$BUILD --build-arg THEPEG_VERSION=${THEPEG_VERSION} --build-arg HERWIG_VERSION=${HERWIG_VERSION}"
 test "$TEST" && BUILD="echo $BUILD"
 
-tag="hepstore/rivet-herwig:${RIVET_VERSION}-${HERWIG_VERSION}"
-$BUILD -t $tag
+tag3="hepstore/rivet-herwig:${RIVET_VERSION}-${HERWIG_VERSION}"
+echo "Building $tag3"
+$BUILD -f Dockerfile.ubuntu -t $tag3
 
-docker tag $tag hepstore/rivet-herwig:$RIVET_VERSION
-docker tag $tag hepstore/rivet-herwig:latest
+echo -e "\n\n"
+
+tag2="hepstore/rivet-herwig:${RIVET_VERSION}-${HERWIG_VERSION}-py2"
+echo "Building $tag2"
+$BUILD -f Dockerfile.ubuntu -t $tag2
+
+docker tag $tag2 hepstore/rivet-herwig:$RIVET_VERSION
+docker tag $tag2 hepstore/rivet-herwig:latest
 
 if [[ "$PUSH" = 1 ]]; then
-    docker push $tag
+    docker push $tag2
     docker push hepstore/rivet-herwig:$RIVET_VERSION
     docker push hepstore/rivet-herwig:latest
 fi
