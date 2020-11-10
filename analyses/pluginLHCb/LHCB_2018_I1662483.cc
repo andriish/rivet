e9fba6f752df2059d69fccf7822f3af3ac3cd177
Andy Buckley <andy.buckley@cern.ch>
2020-11-10 16:14:50 +0000

    Add LHCB_2018_I1662483 (forward dilepton top-pair production cross-section). Thanks to Alex Grecu!

--- analyses/pluginLHCb/LHCB_2018_I1662483.cc
+++ analyses/pluginLHCb/LHCB_2018_I1662483.cc
@@ -11,10 +11,12 @@ namespace Rivet {
 
 
   /// @brief Measurement of forward top pair production in the mu e channel at LHCb
-  /// Both leptons with pt > 20 GeV and in pseudorapidity range 2.0 - 4.5
-  /// Leptons are separated from each other by a dR of 0.1
-  /// Leading jet is required to be a b-jet with pt > 20 GeV and pseudorapidity in range 2.2 - 4.2
-  // Jet is separated from both leptons by dR of 0.5
+  ///
+  /// Both leptons with pt > 20 GeV and in pseudorapidity range 2.0 - 4.5.
+  /// Leptons are separated from each other by a dR of 0.1.
+  /// Leading jet is required to be a b-jet with pt > 20 GeV and pseudorapidity in range 2.2 - 4.2.
+  /// The jet is separated from both leptons by dR of 0.5.
+  ///
   class LHCB_2018_I1662483 : public Analysis {
   public:
 
@@ -30,7 +32,7 @@ namespace Rivet {
 
       // Initialise and register projections
 
-      // the basic final-state projection: 
+      // the basic final-state projection:
       // all final-state particles
       const FinalState fs;
 
@@ -56,11 +58,9 @@ namespace Rivet {
       book(_h_fiducial_xsect ,1,1,1);
     }
 
+
     /// Perform the per-event analysis
     void analyze(const Event& event) {
-
-      /// @todo Do the event by event analysis here
-
 
       // retrieve leptons, sorted by pT
       const FinalState& muons = apply<FinalState>(event, "muons");
@@ -71,27 +71,28 @@ namespace Rivet {
       Jets jets = apply<FastJets>(event, "jets").jetsByPt(Cuts::pT > 20*GeV && Cuts::etaIn(2.2,4.2));
 
       if (jets.empty()) vetoEvent;
-    bool pass = false;
-    for (auto m : muons.particles()) {
-      for (auto e : electrons.particles()){
-        if (deltaR(m.momentum(), e.momentum()) < 0.1 ) continue;
-        vector<Jet> lepton_jets;
-        for ( auto j : jets ){
-          if (deltaR(j.momentum(), m.momentum()) > 0.5 &&
-              deltaR(j.momentum(), e.momentum()) > 0.5)
+      bool pass = false;
+      for (const auto& m : muons.particles()) {
+        for (const auto& e : electrons.particles()) {
+          if (deltaR(m.momentum(), e.momentum()) < 0.1 ) continue;
+          vector<Jet> lepton_jets;
+          for (const auto& j : jets ){
+            if (deltaR(j.momentum(), m.momentum()) > 0.5 &&
+                deltaR(j.momentum(), e.momentum()) > 0.5)
               lepton_jets.push_back(j);
-          if (lepton_jets.size() > 0){
-            for ( auto b : bquarks.particles() ){
-              if ( deltaR(b.momentum(), lepton_jets.at(0).momentum()) < 0.5 )  {
-              pass = true;
+            if (lepton_jets.size() > 0){
+              for (const auto& b : bquarks.particles()) {
+                if ( deltaR(b.momentum(), lepton_jets.at(0).momentum()) < 0.5 )  {
+                  pass = true;
+                }
               }
             }
           }
         }
       }
-    }
+
       // veto event if it doesn't pass our selection
-      if (!pass)  vetoEvent;
+      if (!pass) vetoEvent;
       // append to cross-section
       _h_fiducial_xsect->fill(sqrtS()/GeV);
     }
@@ -102,14 +103,13 @@ namespace Rivet {
       scale(_h_fiducial_xsect, crossSection()/picobarn/sumOfWeights()); // norm to cross section
     }
 
+
     /// Histogram
     Histo1DPtr _h_fiducial_xsect;
 
   };
 
 
-  // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(LHCB_2018_I1662483);
-
 
 }
