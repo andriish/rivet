843f26ab98056d75e721aab04a6ad49dd439385e
Simon Thor <simon11.thor@hotmail.se>
2021-07-19 12:12:29 +0200

    Create file which will contain code for plotting 2D histograms

---
+++ mpl-plotting/rivet_plot2d.py
@@ -0,0 +1,9 @@
+"""Module creates a rivet-style plot as a pdf."""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import yoda
+
+
+def _plot_2Dhist(yaml_file):
+    pass
