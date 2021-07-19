0a8d46b0444c767e2a89646995dd5f7afa016755
Simon Thor <simon11.thor@hotmail.se>
2021-07-19 12:16:42 +0200

    Update function arguments

--- mpl-plotting/rivet_plot2d.py
+++ mpl-plotting/rivet_plot2d.py
@@ -1,9 +1,9 @@
-"""Module creates a rivet-style plot as a pdf."""
+"""Module creates a rivet-style plot with 2D histograms."""
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import yoda
 
 
-def _plot_2Dhist(yaml_file):
+def _plot_2Dhist(hist_data, axes, hist_features, plot_features):
     pass
