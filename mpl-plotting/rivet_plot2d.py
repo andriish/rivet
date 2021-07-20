ceb883e69f77b55a06384164cbec8da214119084
Simon Thor <simon11.thor@hotmail.se>
2021-07-20 03:18:18 +0200

    Implement first working rivet_plot2d

--- mpl-plotting/rivet_plot2d.py
+++ mpl-plotting/rivet_plot2d.py
@@ -6,23 +6,36 @@ import yoda
 
 
 def _plot_projection(hist_data, fig, hist_features, plot_features):
-    axes = mpl.gridspec.gridspec.GridSpec(ncols=len(hist_data), nrows=2 if plot_features['RatioPlot'] else 1)
+    gs = fig.add_gridspec(ncols=len(hist_data), nrows=2 if plot_features.get('RatioPlot', True) else 1)
     colorbar_tick_format = mpl.ticker.ScalarFormatter(useMathText=True)
     colorbar_tick_format.set_powerlimits(
         (-plt.rcParams.get('axes.formatter.min_exponent'), plt.rcParams.get('axes.formatter.min_exponent'))
     )
     
-    for h, ax in zip(hist_data, axes):
+    for i, (h, settings) in enumerate(zip(hist_data, hist_features)):
+        ax = fig.add_subplot(gs[0, i])
         x_edges, y_edges = np.meshgrid(h.xEdges(), h.yEdges())
+        # TODO: should colorbar have min and max for all of them instead? In this case, only one colorbar is used.
         im = ax.pcolormesh(x_edges, y_edges, h.zVals(asgrid=True).T, norm=mpl.colors.Normalize(vmin=h.zMin(), vmax=h.zMax()))
-        fig.colorbar(im, ax=ax, format=colorbar_tick_format)
-        ax.set_title(h['Title'])
+        cbar = fig.colorbar(im, ax=ax, format=colorbar_tick_format)
+        cbar.set_label(plot_features.get('ZLabel'))
+        ax.set(xlabel=plot_features.get('XLabel'), ylabel=plot_features.get('YLabel'), title=settings['Title'])
 
-    return axes
+    if plot_features.get('RatioPlot', True):
+        ref_h, ref_settings = hist_data[0], hist_features[0]
+        for i, h in enumerate(hist_data[1:]):
+            ax = fig.add_subplot(gs[1, i+1])
+            x_edges, y_edges = np.meshgrid(h.xEdges(), h.yEdges())
+            im = ax.pcolormesh(x_edges, y_edges, (h.zVals(asgrid=True) / ref_h.zVals(asgrid=True)).T)
+            cbar = fig.colorbar(im, ax=ax, format=colorbar_tick_format)
+            cbar.set_label(plot_features.get('ZLabel'))
+            ax.set(xlabel=plot_features.get('XLabel'), ylabel=plot_features.get('YLabel'), 
+                title=settings.get('RatioPlotTitle', '{}/{}'.format(settings['Title'], ref_settings['Title'])))
+    fig.tight_layout()
 
 
 def _plot_surface(hist_data, fig, hist_features, plot_features):
-    axes = mpl.gridspec.gridspec.GridSpec(ncols=len(hist_data), nrows=2 if plot_features['RatioPlot'] else 1)
+    axes = mpl.gridspec.gridspec.GridSpec(ncols=len(hist_data), nrows=2 if plot_features.get('RatioPlot', True) else 1)
     
     return axes
 
@@ -45,6 +58,7 @@ def _plot_2Dhist(hist_data, axes, hist_features, plot_features):
         TODO axes will have to be created inside this function later.
     hist_features : dict
         Plot settings for each histogram.
+        TODO: currently only supports the "Title" setting but more should be added. 
     plot_features : dict
         Plot settings for the entire axes.
 
@@ -53,7 +67,7 @@ def _plot_2Dhist(hist_data, axes, hist_features, plot_features):
     None
     """
     # TODO these 2 lines will be removed when axes are always created inside the plotting function
-    fig = axes.get_figure()
+    fig = axes[0].get_figure()
     fig.clf()
     if plot_features.get('2DType', 'projection') == 'projection':
         _plot_projection(hist_data, fig, hist_features, plot_features)
@@ -62,4 +76,4 @@ def _plot_2Dhist(hist_data, axes, hist_features, plot_features):
     elif plot_features.get('2DType') == 'contour':
         _plot_contour(hist_data, fig, hist_features, plot_features)
     else:
-        raise NotImplementedError('Expected 2DType to be "projection", "surface" or "contour" but got {}' % plot_features.get('2DType'))
+        raise NotImplementedError('Expected 2DType to be "projection", "surface" or "contour" but got {}'.format(plot_features.get('2DType')))
