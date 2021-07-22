95cd033955f3aaac5532fbd0b9bd3ce05d693aa8
Simon Thor <simon11.thor@hotmail.se>
2021-07-22 20:24:06 +0200

    Add colorbar (not optimal or refactored)

--- mpl-plotting/rivet_plot2d.py
+++ mpl-plotting/rivet_plot2d.py
@@ -4,17 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 # TODO
 #   Only have 1 colorbar per row (?)
+#     Spacing between plots and colorbar
 #   Implement ShowZero
-#   Implment contour plot
+#   Implement contour plot
 #   Should the ratio plots touch the 2D histogram?
-#   Spacing between plots and colorbar
-def _configure_plot(ax, plot_features, hist_data):
-    # TODO: optimize using e.g. sharex.
-    # TODO: Different settings for ratio and non-ratio?
-    ax.set_xlabel(plot_features.get('XLabel'))
-    ax.set_ylabel(plot_features.get('YLabel'), loc='top')
-    ax.set_title(plot_features.get('Title'), loc='left')
+#   Make separate mplstyle for 2d plots?
 
+def _configure_plot(ax, plot_features, hist_data, hist_settings=None):
+    # TODO: Different settings for ratio and non-ratio?
+    if hist_settings is None:
+        hist_settings = {}
+    ax.set_ylabel(plot_features.get('YLabel'), loc='top')
+    ax.set_title(hist_settings.get('Title'), loc='left')
     # TODO: check that this is ok. Maybe more customization is needed.
     for axis_name in 'XY':
         min_lim = plot_features.get(axis_name + 'Min', min(getattr(h, axis_name.lower() + 'Min')() for h in hist_data))
@@ -29,9 +30,6 @@ def _configure_plot(ax, plot_features, hist_data):
     if plot_features.get('LogY'):
         ax.set_yscale('log')
         ax.yaxis.set_major_locator(mpl.ticker.LogLocator(numticks=np.inf))
-        ax.yaxis.set_minor_locator(mpl.ticker.LogLocator(
-            base=10.0, subs=[i for i in np.arange(0, 1, 0.1)], numticks=np.inf))
-    # TODO: LogZ
 
     # Set tick marks frequency
     if plot_features.get('XMajorTickMarks') is not None and not plot_features.get('LogX'):
@@ -64,7 +62,51 @@ def _configure_plot(ax, plot_features, hist_data):
         ax.set_yticks(plot_features.get('YCustomMinorTicks'), minor=True)
     if plot_features.get('PlotXTickLabels') == 0:
         ax.set_xticklabels([])
+
     # TODO: Z tick labels
+    
+
+# TODO this might not be useful if all should have separate colorbars
+def _create_norm(plot_features, default_zmin, default_zmax):
+    zmin = plot_features.get('ZMin', default_zmin)
+    zmax = plot_features.get('ZMax', default_zmax)
+
+    # Set log scale
+    if plot_features.get('LogZ'):
+        norm = mpl.colors.LogNorm(vmin=zmin, vmax=zmax)
+    else:
+        norm = mpl.colors.Normalize(vmin=zmin, vmax=zmax)
+    return norm
+
+
+def _create_colorbar(ax, norm, plot_features):
+    colorbar_tick_format = mpl.ticker.ScalarFormatter(useMathText=True)
+    colorbar_tick_format.set_powerlimits(
+        (-plt.rcParams.get('axes.formatter.min_exponent'), plt.rcParams.get('axes.formatter.min_exponent'))
+    )
+    cbar = ax.get_figure().colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=plt.rcParams['image.cmap']), 
+        cax=ax, orientation='vertical', label=plot_features.get('ZLabel'), format=colorbar_tick_format)
+
+    # Set tick marks frequency
+    if 'ZMinorTickMarks' in plot_features and not plot_features.get('LogZ'):
+        ax.yaxis.set_minor_locator(mpl.ticker.AutoMinorLocator(
+            1+plot_features.get('ZMinorTickMarks')))
+
+    if 'ZMajorTickMarks' in plot_features and not plot_features.get('LogZ'):
+        base = plot_features.get('ZMajorTickMarks')*10**(int(np.log10(zmax))-1)
+        ax.yaxis.set_major_locator(mpl.ticker.MultipleLocator(base))
+
+    if plot_features.get('PlotZTickLabels') == 0:
+        ax.set_yticklabels([])
+
+    # Add custom ticks
+    if 'ZCustomMajorTicks' in plot_features:
+        cbar.set_ticks(plot_features.get('ZCustomMajorTicks')[::2])
+        cbar.set_ticklabels(plot_features.get('ZCustomMajorTicks')[1::2])
+    if plot_features.get('ZCustomMinorTicks') is not None:
+        ax.yaxis.set_ticks(plot_features.get('ZCustomMinorTicks'), minor=True)
+
+    return cbar
 
 
 def _plot_projection(hist_data, fig, hist_features, plot_features):
@@ -73,27 +115,26 @@ def _plot_projection(hist_data, fig, hist_features, plot_features):
     nrows = 2 if plot_ratio else 1
     # TODO Try different values for size of colorbar
     width_ratios = np.array([1] * len(hist_data) + [0.0526])
-    fig.set_size_inches(np.sum(width_ratios) * plt.rcParams['figure.figsize'][0], plt.rcParams['figure.figsize'][1] * ncols)
+    # TODO hopefully remove constant
+    fig.set_size_inches(np.sum(width_ratios) * plt.rcParams['figure.figsize'][0] * 1.5, plt.rcParams['figure.figsize'][1] * ncols)
     
     gs = fig.add_gridspec(ncols=ncols, nrows=nrows, width_ratios=width_ratios)
-    colorbar_tick_format = mpl.ticker.ScalarFormatter(useMathText=True)
-    colorbar_tick_format.set_powerlimits(
-        (-plt.rcParams.get('axes.formatter.min_exponent'), plt.rcParams.get('axes.formatter.min_exponent'))
-    )
     
     zmin, zmax = min(h.zMin() for h in hist_data), max(h.zMax() for h in hist_data)
-
+    normalizer = _create_norm(plot_features, min(h.zMin() for h in hist_data), max(h.zMax() for h in hist_data))
+    
     for i, (h, settings) in enumerate(zip(hist_data, hist_features)):
         ax = fig.add_subplot(gs[0, i])
         x_edges, y_edges = np.meshgrid(h.xEdges(), h.yEdges())
         # TODO: should colorbar have min and max for all of them instead? In this case, only one colorbar is used.
-        im = ax.pcolormesh(x_edges, y_edges, h.zVals(asgrid=True).T, norm=mpl.colors.Normalize(vmin=zmin, vmax=zmax))
+        im = ax.pcolormesh(x_edges, y_edges, h.zVals(asgrid=True).T, norm=normalizer)    
         _configure_plot(ax, plot_features, hist_data)
-    cbar = fig.colorbar(im, cax=fig.add_subplot(gs[0, -1]), format=colorbar_tick_format)
-    cbar.set_label(plot_features.get('ZLabel'))
+        ax.set_title(settings['Title'])
+
+    _create_colorbar(fig.add_subplot(gs[0, -1]), normalizer, plot_features)
 
     if plot_features.get('RatioPlot', True):
-        ref_h, ref_settings = hist_data[0], hist_features[0]
+        ref_h = hist_data[0]
         zmin = zmax = np.nan
         for i, h in enumerate(hist_data[1:]):
             ax = fig.add_subplot(gs[1, i+1])
@@ -104,11 +145,18 @@ def _plot_projection(hist_data, fig, hist_features, plot_features):
             zmin_new, zmax_new = np.nanmin(z_ratio), np.nanmax(z_ratio)
             zmin, zmax = np.nanmin([zmin, zmin_new]), np.nanmax([zmax, zmax_new])            
             im = ax.pcolormesh(x_edges, y_edges, z_ratio)
-            _configure_plot(ax, plot_features, hist_data)
-
+            # TODO format ratio plot
+            #  axis limits cannot be set here since it is unknown
+        # TODO: format colorbar
+        # TODO move
+        colorbar_tick_format = mpl.ticker.ScalarFormatter(useMathText=True)
+        colorbar_tick_format.set_powerlimits(
+            (-plt.rcParams.get('axes.formatter.min_exponent'), plt.rcParams.get('axes.formatter.min_exponent'))
+        )
+    
         cbar = fig.colorbar(im, cax=fig.add_subplot(gs[1, -1]), format=colorbar_tick_format)
         cbar.set_label(plot_features.get('ZLabel'))
-            
+
 
 def _plot_surface(hist_data, fig, hist_features, plot_features):
     """Plot 3D plots with a surface representing histograms.
