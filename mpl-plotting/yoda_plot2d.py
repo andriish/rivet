ffffbc71c3e78c14b8df24dad5009048c43fd8ee
Simon Thor <simon11.thor@hotmail.se>
2021-08-10 12:04:42 +0200

    Add format_axis and finish writing kwargs. Warning: the functions have not been tested

--- mpl-plotting/yoda_plot2d.py
+++ mpl-plotting/yoda_plot2d.py
@@ -40,6 +40,67 @@ def _scatter_to_2d(hs, xy_type):
     return x, y, z
 
 
+def format_axis(axis_name, ax=None, label=None, lim=None, log=False, 
+    major_ticks=None, minor_ticks=None, custom_major_ticks=None, custom_minor_ticks=None,
+    plot_ticklabels=True):
+    """Format an axis (e.g. x axis, **NOT** an Axes object) based on the inputs.
+    If an optional input variable is None, the option will be ignored, unless a different behavior is specified.
+    
+    Parameters
+    ----------
+    axis_name : str
+        x, y or z. Either upper or lower case.
+    ax : matplotlib.axes.Axes
+        The Axes object containing the axis. If None, uses plt.gca()
+    label : str, optional
+        Axis label.
+    lim : tuple, optional
+        Lower and upper limits of axis, in that order.
+    log : bool, optional
+        If True, set the scale of the axis to log scale. By default False
+    major_ticks : int, optional
+        Digit of the major ticks, by default None
+    minor_ticks : int, optional
+        Number of minor ticks, by default None
+    custom_major_ticks : Sequence, optional
+        Location of tick, followed by the corresponding tick label.
+    custom_minor_ticks : Sequence[float], optional
+        A list of the locations of minor ticks at arbitrary positions. 
+    plot_ticklabels : bool, optional
+        If False, do not plot any tick labels, by default True
+    """
+    if ax is None:
+        ax = plt.gca()
+    
+    axis_name = axis_name.lower()
+    getattr(ax, 'set_{}label'.format(axis_name))(label)
+    getattr(ax, 'set_{}lim'.format(axis_name))(lim)
+
+    # Set log scale
+    if log:
+        ax.set(**{axis_name + 'scale': 'log'})
+        getattr(ax, axis_name + 'axis').set_major_locator(mpl.ticker.LogLocator(numticks=np.inf))
+    else:
+        # Set tick marks frequency
+        if major_ticks:
+            base = major_ticks*10**(int(np.log10(lim[1]))-1)
+            getattr(ax, axis_name + 'axis').set_major_locator(mpl.ticker.MultipleLocator(base))
+        
+        if minor_ticks:
+            getattr(ax, axis_name + 'axis').set_minor_locator(mpl.ticker.AutoMinorLocator(
+                1+minor_ticks))
+    
+    # Add custom ticks
+    if custom_major_ticks:
+        ax.set(**{axis_name + 'ticks': custom_major_ticks[::2],
+            axis_name + 'ticklabels': custom_major_ticks[1::2]})
+        getattr(ax, 'set_{}ticks'.format(axis_name))([], minor=True)  # Turn off minor ticks
+    if custom_minor_ticks:
+        getattr(ax, 'set_{}ticks'.format(axis_name))(custom_minor_ticks, minor=True)
+    if plot_ticklabels == 0:
+        ax.set(**{axis_name + 'ticklabels': []})
+
+
 def _create_norm(zmin, zmax, log):
     """Small wrapper function to create mpl norm that is used as the limits for the colorbar and pcolormesh."""
     if log:
@@ -76,8 +137,7 @@ def _add_colorbar(norm, cmap, ax=None, *args, **kwargs):
     return cbar
 
 
-def proj(yoda_hist, ax=None, zmin=None, zmax=None, logz=False, showzero=True, 
-    colorbar=True, cmap=None, pcm_kw=None, cbar_kw=None):
+def proj(yoda_hist, ax=None, showzero=True, colorbar=True, cmap=None, **kwargs):
     """Plot a 2D projection plot of a yoda Scatter3D object.
 
     Parameters
@@ -90,46 +150,46 @@ def proj(yoda_hist, ax=None, zmin=None, zmax=None, logz=False, showzero=True,
     """
     x_edges, y_edges, z_vals = _scatter_to_2d(yoda_hist, 'edge')
 
-    return _proj_base(x_edges, y_edges, z_vals, ax, zmin, zmax, logz, showzero, colorbar, cmap, pcm_kw, cbar_kw)
+    return _proj_base(x_edges, y_edges, z_vals, ax=ax, showzero=showzero, colorbar=colorbar, cmap=cmap, **kwargs)
 
 
-def ratio_proj(yoda_hist, ref_hist, ax=None, zmin=None, zmax=None, logz=False, showzero=True, 
-    colorbar=True, cmap=None, pcm_kw=None, cbar_kw=None):
+def ratio_proj(yoda_hist, ref_hist, ax=None, showzero=True, colorbar=True, cmap=None, **kwargs):
 
     x_edges, y_edges, mc_z = _scatter_to_2d(yoda_hist, 'edge')
     ref_z = ref_hist.zVals().reshape(mc_z.shape) # TODO might not work
 
     z_ratio = mc_z / ref_z
     
-    return _proj_base(x_edges, y_edges, z_ratio, ax, zmin, zmax, logz, showzero, colorbar, cmap, pcm_kw, cbar_kw)
+    return _proj_base(x_edges, y_edges, z_ratio, ax=ax, showzero=showzero, colorbar=colorbar, cmap=cmap, **kwargs)
     
 
-def _proj_base(x_edges, y_edges, z_vals, ax=None, zmin=None, zmax=None, logz=False, showzero=True, 
-    colorbar=True, cmap=None, pcm_kw=None, cbar_kw=None):
+def _proj_base(x_edges, y_edges, z_vals, ax=None, showzero=True, colorbar=True, cmap=None, 
+    pcm_kw=None, cbar_kw=None, xaxis_kw=None, yaxis_kw=None, zaxis_kw=None):
     if ax is None:
         ax = plt.gca()
-    if pcm_kw is None:
-        pcm_kw = {}
-    if cbar_kw is None:
-        cbar_kw = {}
+    pcm_kw, cbar_kw, xaxis_kw, yaxis_kw, zaxis_kw = [{} if kw is None else kw for kw in (pcm_kw, cbar_kw, xaxis_kw, yaxis_kw, zaxis_kw)]
 
-    norm = _create_norm(zmin, zmax, logz)
+    norm = _create_norm(*zaxis_kw.get('lim', ()), zaxis_kw.get('log'))
     
     if not showzero:
         z_vals[z_vals==0] = np.nan
         
     im = ax.pcolormesh(x_edges, y_edges, z_vals, norm=norm, cmap=cmap, **pcm_kw)
 
+    # TODO ask for preference about keeping format_axis in here
+    format_axis('x', ax, **xaxis_kw)
+    format_axis('y', ax, **yaxis_kw)
+    
     if colorbar:
         cbar = _add_colorbar(norm, cmap=cmap, ax=ax, **cbar_kw)
+        format_axis('y', cbar.ax, **zaxis_kw)
         return im, cbar
     
-    # TODO keep format_axis inside here. Ask for preference
-    
+
     return im
 
 
-def surf(yoda_hist, ax=None, zmin=None, zmax=None, showzero=True, cmap=None, psurf_kw=None):
+def surf(yoda_hist, ax=None, showzero=True, cmap=None, **kwargs):
     """Plot 3D plot with a surface representing a histogram.
 
     Parameters
@@ -144,38 +204,29 @@ def surf(yoda_hist, ax=None, zmin=None, zmax=None, showzero=True, cmap=None, psu
     """
     x, y, z = list(_scatter_to_2d(yoda_hist, 'mid'))
 
-    return _surf_base(x, y, z, ax, zmin, zmax, showzero, cmap, psurf_kw)
+    return _surf_base(x, y, z, ax, showzero=showzero, cmap=cmap, **kwargs)
 
 
-def ratio_surf(ref_hist, yoda_hist, ax=None, zmin=None, zmax=None, showzero=True, cmap=None, psurf_kw=None):
-
+def ratio_surf(ref_hist, yoda_hist, ax=None, showzero=True, cmap=None, **kwargs):
     x, y, mc_z = _scatter_to_2d(yoda_hist, 'mid')
     ref_z = ref_hist.zVals().reshape(mc_z.shape)
     z = mc_z / ref_z
-    return _surf_base(x, y, z, ax, zmin, zmax, showzero, cmap, psurf_kw)
+    return _surf_base(x, y, z, ax, showzero=showzero, cmap=cmap, **kwargs)
 
-def _surf_base(x, y, z, ax=None, zmin=None, zmax=None, showzero=True, cmap=None, psurf_kw=None):
+
+def _surf_base(x, y, z, ax=None, showzero=True, cmap=None, psurf_kw=None, xaxis_kw=None, yaxis_kw=None, zaxis_kw=None, elev=None, azim=None):
     if ax is None:
         ax = plt.gca(projection='3d')
+    psurf_kw, xaxis_kw, yaxis_kw, zaxis_kw = [{} if kw is None else kw for kw in (psurf_kw, xaxis_kw, yaxis_kw, zaxis_kw)]
 
     if not showzero:
         z[z==0] = np.nan
     
-    im = ax.plot_surface(x, y, z, cmap=cmap)
+    im = ax.plot_surface(x, y, z, cmap=cmap, **psurf_kw)
     
-    # If an axis is log/scaled, change axis scale and change axis limits and ticks accordingly.
-    # See Notes in docstring.
-    for i, (axis_name, lim) in enumerate(zip('XYZ', ((plot_features.get('XMin'), plot_features.get('XMax')), 
-                                                     (plot_features.get('YMin'), plot_features.get('YMax')), 
-                                                     (zmin, zmax)))):
-        if plot_features.get('Log' + axis_name):
-            _set_log_axis_3d(axis_name.lower(), ax)
-            log_lim = [np.log10(j) if j is not None else j for j in lim]
-            format_axis(axis_name, ax, label=preprocess(plot_features.get(axis_name + 'Label')), lim=log_lim, custom_major_ticks=_preprocess_custom_ticks(plot_features.get(axis_name + 'CustomMajorTicks')), custom_minor_ticks=plot_features.get(axis_name+'CustomMinorTicks'), plot_ticklabels=plot_features.get('Plot{}TickLabels'.format(axis_name)))
-        else:
-            format_axis(axis_name, ax, preprocess(plot_features.get(axis_name + 'Label')), lim=lim, major_ticks=plot_features.get(axis_name + 'MajorTickMarks'), minor_ticks=plot_features.get(axis_name + 'MinorTickMarks'), custom_major_ticks=_preprocess_custom_ticks(plot_features.get(axis_name + 'CustomMajorTicks')), custom_minor_ticks=plot_features.get(axis_name + 'CustomMinorTicks'), plot_ticklabels=plot_features.get('Plot{}TickLabels'.format(axis_name)))
+    for axis_name, format_axis_kw in zip('xyz', (xaxis_kw, yaxis_kw, zaxis_kw)):
+        format_axis(axis_name, ax, **format_axis_kw)
 
-    # TODO rename keywords?
-    ax.view_init(elev=plot_features.get('3DElev'), azim=plot_features.get('3DAzim'))
+    ax.view_init(elev=elev, azim=azim)
 
     return im
