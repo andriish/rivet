1619439bd6af2ed029531f4ed0d81d192c974925
Andy Buckley <andy.buckley@cern.ch>
2021-10-04 16:26:37 +0100

    Latest hepbase build tweaks

--- docker/hepbase/build.sh
+++ docker/hepbase/build.sh
@@ -40,11 +40,11 @@ for vhepmc in 3.2.4  2.06.11; do
         test "$PUSH" = 1 && docker push $tag && sleep 1m
         echo -e "\n\n\n"
 
-        # echo "@@ $MSG on Ubuntu with GCC compilers and Python 2"
-        # tag=hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc:0:1}-py2$TEXSUFFIX
-        # $BUILD . -f Dockerfile.ubuntu-py2 $GCCARGS -t $tag
-        # test "$PUSH" = 1 && docker push $tag && sleep 1m
-        # echo -e "\n\n\n"
+        echo "@@ $MSG on Ubuntu with GCC compilers and Python 2"
+        tag=hepstore/hepbase-ubuntu-gcc-hepmc${vhepmc:0:1}-py2$TEXSUFFIX
+        $BUILD . -f Dockerfile.ubuntu-py2 $GCCARGS -t $tag
+        test "$PUSH" = 1 && docker push $tag && sleep 1m
+        echo -e "\n\n\n"
 
     done
 done
