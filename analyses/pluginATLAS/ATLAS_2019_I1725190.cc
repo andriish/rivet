b492dd636662e9379b8626b11d1b7e56b60c0930
Jon Butterworth <j.butterworth@ucl.ac.uk>
2021-10-12 08:07:10 +0100

    fix normalisation

--- analyses/pluginATLAS/ATLAS_2019_I1725190.cc
+++ analyses/pluginATLAS/ATLAS_2019_I1725190.cc
@@ -134,10 +134,13 @@ namespace Rivet {
     }
 
 
-    /// Normalise histograms etc., after the run
+    /// Normalise histograms etc., after the run.
+    //  The plot is expressed as number of events in a 10 GeV bin.
+    //  Multiply by luminosity*cross section (in same units!) to get number
+    //  of events a 1 GeV bin, then by 10 to get number of events in a 10 GeV.
     void finalize() {
-      scale(_h_mee, crossSection()*luminosity()/femtobarn/sumOfWeights());
-      scale(_h_mmm, crossSection()*luminosity()/femtobarn/sumOfWeights());
+      scale(_h_mee, 10.*crossSection()*luminosity()/sumOfWeights());
+      scale(_h_mmm, 10.*crossSection()*luminosity()/sumOfWeights());
     }
 
     //@}
