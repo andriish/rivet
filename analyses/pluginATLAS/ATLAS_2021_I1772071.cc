180351ea361d3e742653cb4f2251945adc7ae71b
Christian Gutschow <chris.g@cern.ch>
2021-10-18 11:31:33 +0100

    use canonical macro names

--- analyses/pluginATLAS/ATLAS_2021_I1772071.cc
+++ analyses/pluginATLAS/ATLAS_2021_I1772071.cc
@@ -14,7 +14,7 @@ namespace Rivet {
   public:
 
     // Constructor
-    DEFAULT_RIVET_ANALYSIS_CTOR(ATLAS_2021_I1772071);
+    RIVET_DEFAULT_ANALYSIS_CTOR(ATLAS_2021_I1772071);
 
     // Book histograms and initialise projections before the run
     void init() {
@@ -197,6 +197,6 @@ namespace Rivet {
   };
 
   // The hook for the plugin system
-  DECLARE_RIVET_PLUGIN(ATLAS_2021_I1772071);
+  RIVET_DECLARE_PLUGIN(ATLAS_2021_I1772071);
 
 }
