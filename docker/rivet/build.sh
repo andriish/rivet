792e71c5dd4773d998a96917d56a0797ca149904
Andy Buckley <andy.buckley@cern.ch>
2021-12-02 16:24:56 +0000

    Remove YODA from hepbase, and update Rivet Dockerfiles to compensate

--- docker/rivet/build.sh
+++ docker/rivet/build.sh
@@ -6,10 +6,13 @@ BUILD="docker build . -f Dockerfile"
 
 test "$FORCE" && BUILD="$BUILD --no-cache"
 
+YODA_BRANCH=yoda-1.9.4
+
 ## Last branch name -> latest
 for RIVET_BRANCH in release-3-1-x rivet-3.1.5; do
     RIVET_VERSION=${RIVET_BRANCH#rivet-}
 
+    BUILD="$BUILD --build-arg YODA_BRANCH=$YODA_BRANCH" # --squash"
     BUILD="$BUILD --build-arg RIVET_BRANCH=$RIVET_BRANCH" # --squash"
     test "$TEST" && BUILD="echo $BUILD"
 
