d731de0d37546edcd22d1d33361016c5e4388430
Andy Buckley <andy.buckley@cern.ch>
2022-05-18 09:25:46 +0100

    Add rivet-sherpa build script

---
+++ docker/rivet-sherpa/build.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+set -e
+
+RIVET_VERSION=3.1.6
+SHERPA_VERSION=2.2.11
+
+BUILD="docker build ."
+
+test "$FORCE" && BUILD="$BUILD --no-cache"
+
+BUILD="$BUILD --build-arg RIVET_VERSION=${RIVET_VERSION}"
+BUILD="$BUILD --build-arg SHERPA_VERSION=${SHERPA_VERSION}"
+test "$TEST" && BUILD="echo $BUILD"
+
+tag="hepstore/rivet-sherpa:${RIVET_VERSION}-${SHERPA_VERSION}"
+echo "Building $tag"
+$BUILD -f Dockerfile -t $tag
+
+docker tag $tag hepstore/rivet-sherpa:$RIVET_VERSION
+if [[ "$LATEST" = 1 ]]; then
+    docker tag $tag hepstore/rivet-sherpa:latest
+fi
+
+if [[ "$PUSH" = 1 ]]; then
+    docker push $tag
+    sleep 1m
+    docker push hepstore/rivet-sherpa:$RIVET_VERSION
+    if [[ "$LATEST" = 1 ]]; then
+        sleep 1m
+        docker push hepstore/rivet-sherpa:latest
+    fi
+fi
