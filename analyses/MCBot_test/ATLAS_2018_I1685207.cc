faa2d88b5c95ed70e89168876fa1b3e283c7763b
VNovackova <2442590n@student.gla.ac.uk>
2022-07-01 17:21:30 +0100

    Adding ATLAS_2018_I1685207.cc file

--- analyses/MCBot_test/ATLAS_2018_I1685207.cc
+++ analyses/MCBot_test/ATLAS_2018_I1685207.cc
@@ -16,9 +16,10 @@
 #include "Rivet/Tools/JetSmearingFunctions.hh"
 #include "Rivet/Math/MathUtils.hh"
 #include "fastjet/contrib/VariableRPlugin.hh"
-#include "fastjet/contrib/Nsubjettiness.hh"
 #include "fastjet/tools/Filter.hh"
 #include <fstream>
+
+namespace Rivet {
 
 //trims reclustered jets by removing all subjets with pt < minpt. Does not work in place,
 //has seperate inout&output. Also returns a vector of new constituents so we definitely save this
@@ -27,7 +28,7 @@
 void naive_RC_trimmer(const Rivet::PseudoJets& input, Rivet::PseudoJets& output,
                        double pt_frac, vector<Rivet::PseudoJets>& new_constituents){
   output.clear();
-  for (const Rivet::PseudoJet& j : input){
+  for (const PseudoJet& j : input){
     std::vector<size_t> to_keep;
     double min_pt = j.pt() * pt_frac;
     for (size_t i = 0; i < j.constituents().size(); ++i){
@@ -38,8 +39,8 @@ void naive_RC_trimmer(const Rivet::PseudoJets& input, Rivet::PseudoJets& output,
     //TODO this is overcomplicated.
     
     if (to_keep.size() > 0){
-      Rivet::PseudoJet jet;
-      Rivet::PseudoJets constits;
+      PseudoJet jet;
+      PseudoJets constits;
       for (const size_t i : to_keep){
         jet+=j.constituents()[i];
         constits.push_back(j.constituents()[i]);
@@ -50,9 +51,8 @@ void naive_RC_trimmer(const Rivet::PseudoJets& input, Rivet::PseudoJets& output,
   }
 }
 
-
 //Numbers match MCBot_tagtype.
-//TOOD: probably should only have one type of enum
+//TODO: probably should only have one type of enum
 enum class DNN_Category{
   V_tag=2,
   H_tag=3,
@@ -60,18 +60,20 @@ enum class DNN_Category{
   bkg_tag=0
 };
 
+
+
 //Very rough and hacky
 //Find all Vector bosons, higgs, and tops that are not children of themselves.
 //TODO: implement as a projection?
-void getVHandtop_fromEvent(std::vector<Rivet::Particle>& ps, const Rivet::Event& ge){
+void getVHandtop_fromEvent(std::vector<Particle>& ps, const Event& ge){
   for (const auto& p : ge.allParticles()){
     int pid = p.pid();
     //TODO: Should there also be a status code check?
     //If its Vht, check its parents aren't the same particle to avoid including what is really the same particle many times.
     if (abs(pid) == 23 || abs(pid) == 24 || abs(pid) == 6 || abs(pid) == 25){
-      Rivet::Particles parents = p.parents();
+      Particles parents = p.parents();
       auto it = std::find_if(parents.begin(), parents.end(),
-                             [pid](const Rivet::Particle &p){return p.pid() == pid;});
+                             [pid](const Particle &p){return p.pid() == pid;});
       if (it == parents.end()){
         ps.push_back(p);
       }
@@ -80,40 +82,8 @@ void getVHandtop_fromEvent(std::vector<Rivet::Particle>& ps, const Rivet::Event&
 
 }
 
-// 
-DNN_Category getTrueDNNtag(const Rivet::PseudoJet& pj, const Rivet::Particles& comp){
-  for (const Rivet::Particle& comp_p : comp){
-    //Jets need to be within deltaR 0.75rho/pT
-    //TODO: I guess rho is the same from vRC jet reclustering but is it the particle or jet pT?
-    // I think jet but not 100% sure.
-    //TODO: In the paper when they ran the simulation to train the DNN, they discarded events with
-    // with multiple "true" matches. But what is correct in this context.
-    // For now I'm going with quick and dirty "first one in range works";
-    if (Rivet::deltaR(Rivet::momentum3(pj), Rivet::momentum3(comp_p.pseudojet())) < 315*Rivet::GeV/pj.pt()){
-      if (comp_p.abspid() == 23 || comp_p.abspid() == 24){
-        //std::cout << "TAGGING Vector Boson" << std::endl;
-        return DNN_Category::V_tag;
-      }
-      if (comp_p.abspid() == 25){
-        ///std::cout << "TAGGING Higgs" << std::endl;
-        return DNN_Category::H_tag;
-      }
-      if (comp_p.abspid() == 6){
-        ///std::cout << "TAGGING top" << std::endl;
-        return DNN_Category::top_tag;
-      }
-    }
-  }
-
-  //std::cout << "TAGGING bkg" << std::endl;
-  return DNN_Category::bkg_tag;
-}
-
-
-// angular smearing function: building on JET_SMEAR_ATLAS_RUN2 (copy of run 1)
-namespace Rivet {
-
-Jet JET_SMEAR_ANGULAR2(const Jet& j) {
+// angular smearing function: building on JET_SMEAR_ATLAS_RUN2 
+Jet JET_SMEAR_ANGULAR(const Jet& j) {
   // Jet energy resolution lookup
   //   original -- Implemented by Matthias Danninger for GAMBIT, based roughly on
   //   https://atlas.web.cern.ch/Atlas/GROUPS/PHYSICS/CONFNOTES/ATLAS-CONF-2015-017/
@@ -127,46 +97,14 @@ Jet JET_SMEAR_ANGULAR2(const Jet& j) {
 
   // Smear by a Gaussian centered on 1 with width given by the (fractional) resolution
   /// @todo Is this the best way to smear? Should we preserve the energy, or pT, or direction?
-  const double fsmear = max(randnorm(1., resolution), 0.); // use this for smearing the energy...
-  const double mass = j.mass2() > 0 ? j.mass() : 0; //< numerical carefulness... is it correct to calculate the mass here? mass vs smeared energy inconsistency???
+  const double fsmear = max(randnorm(1., resolution), 0.); 
+  const double mass = j.mass2() > 0 ? j.mass() : 0; //< numerical carefulness...
   
-  Jet j1(Rivet::FourMomentum::mkXYZM(j.px()*fsmear, j.py()*fsmear, j.pz()*fsmear, mass));
+  Jet j1(FourMomentum::mkXYZM(j.px()*fsmear, j.py()*fsmear, j.pz()*fsmear, mass));
 
-  // smearing in eta-phi ...
-
+  // smearing in eta-phi -- customize the standard deviation in randnorm...
   double dsmear = max(randnorm(0., 0.1), 0.);
-  double theta = rand01() * M_2_PI; // why 2/pi instead of 2pi??
-  // double theta = 2* rand01() * M_PI; // alternatively??
-    
-  
-  return Jet(FourMomentum::mkEtaPhiME(j.eta()+dsmear*cos(theta), mapAngle0To2Pi(j.phi()+dsmear*sin(theta)), j1.mass(), j1.E()));  
-}
-
-Jet JET_SMEAR_ANGULAR3(const Jet& j) {
-  // Jet energy resolution lookup
-  //   original -- Implemented by Matthias Danninger for GAMBIT, based roughly on
-  //   https://atlas.web.cern.ch/Atlas/GROUPS/PHYSICS/CONFNOTES/ATLAS-CONF-2015-017/
-  //   Parameterisation can be still improved, but eta dependence is minimal
-  /// @todo Also need a JES uncertainty component?
-  static const vector<double> binedges_pt = {0., 50., 70., 100., 150., 200., 1000., 10000.};
-  static const vector<double> jer = {0.145, 0.115, 0.095, 0.075, 0.07, 0.05, 0.04, 0.04}; //< note overflow value
-  const int ipt = binIndex(j.pt()/GeV, binedges_pt, true);
-  if (ipt < 0) return j;
-  const double resolution = jer.at(ipt);
-
-  // Smear by a Gaussian centered on 1 with width given by the (fractional) resolution
-  /// @todo Is this the best way to smear? Should we preserve the energy, or pT, or direction?
-  const double fsmear = max(randnorm(1., resolution), 0.); // use this for smearing the energy...
-  const double mass = j.mass2() > 0 ? j.mass() : 0; //< numerical carefulness... is it correct to calculate the mass here? mass vs smeared energy inconsistency???
-  
-  Jet j1(Rivet::FourMomentum::mkXYZM(j.px()*fsmear, j.py()*fsmear, j.pz()*fsmear, mass));
-
-  // smearing in eta-phi ...
-
-  double dsmear = max(randnorm(0., 1.), 0.);
-  double theta = rand01() * M_2_PI; // why 2/pi instead of 2pi??
-  // double theta = 2* rand01() * M_PI; // alternatively??
-    
+  double theta = rand01() * M_2_PI;
   
   return Jet(FourMomentum::mkEtaPhiME(j.eta()+dsmear*cos(theta), mapAngle0To2Pi(j.phi()+dsmear*sin(theta)), j1.mass(), j1.E()));  
 }
@@ -188,7 +126,7 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
     /// Book histograms and initialise projections before the run
     void init() {
 
-      // BCG = background mode, SGN = signal mode   
+      // BCG = background mode, SGN = signal mode (V, H, top)  
       // default to signal mode
 
       _mode = 1; 
@@ -223,9 +161,9 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       // No smearing:
 	    //SmearedJets SSj(Sj, JET_SMEAR_IDENTITY, JET_BTAG_EFFS(0.77, 1./6.2, 1./134));
       /// Energy-resolution smearing only:
-	    //SmearedJets SSj(Sj, JET_SMEAR_ATLAS_RUN2, JET_BTAG_EFFS(0.77, 1./6.2, 1./134));
+	    SmearedJets SSj(Sj, JET_SMEAR_ATLAS_RUN2, JET_BTAG_EFFS(0.77, 1./6.2, 1./134));
       /// @todo Also look into angular smearing? Need a custom smearing function, building on the ATLAS R2
-      SmearedJets SSj(Sj, JET_SMEAR_ANGULAR3, JET_BTAG_EFFS(0.77, 1./6.2, 1./134));
+      //SmearedJets SSj(Sj, JET_SMEAR_ANGULAR, JET_BTAG_EFFS(0.77, 1./6.2, 1./134));
     	declare(SSj, "smearedSjet");
       
       // pTmiss
@@ -261,17 +199,13 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       book(_valBins["HH_1t_1b"], "HH_1t_1b");
       book(_valBins["XX_2t_1b"], "XX_2t_1b");
 
-      // discriminant function histograms
+      // Histograms for distriminant functions
       book(_h["PV"], "PV",45,-3,1.5);
       book(_h["PH"], "PH",55,-3,2.5);
       book(_h["Pt"], "Pt",55,-3,2.5);      
 
-      // Les Houches angularity, number of constituent small-R jets
+      // Les Houches angularity
       book(_h["LHA"],"LHA",200, 0, 1);
-      book(_h["Nconsts"],"Nconsts", 10,0,10);
-      // book(_h["tau21"], "tau21", 100, 0, 1);
-      // book(_h["tau32"], "tau32", 100, 0, 1);
-      
 
       //Find the json file
       const std::string nn_datafilename = "ATLAS_2018_I1685207.nn.json.yoda";
@@ -314,7 +248,7 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       }
       double ETmiss = pTmiss.pT();
       
-      
+
       //Recluster small jets using variable R
       // rho= 315GeV, 0.4<Reff<1.2
       //TODO: probably more efficient to move after pre-selection.
@@ -322,15 +256,14 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       fastjet::JetDefinition jdef(variableRplugin);
       ClusterSequence cseq(smeared_small_jets, jdef);
       PseudoJets VRC_jets = cseq.inclusive_jets();
+    
       
-      
-      //TODO there's got to be an easier way than this mess.
+      // Trimming
       PseudoJets TrimmedVRCjets;
       vector<PseudoJets> NewConstits;
       naive_RC_trimmer(VRC_jets, TrimmedVRCjets, 0.05, NewConstits);
       
       // pT, eta, and mass cuts
-
       PseudoJets FilteredVRCjets;
       vector<PseudoJets> FilteredNewConstits;
 
@@ -348,9 +281,8 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
         vetoEvent;
       }    
       
-      // defining signal as an input for further analysis and filling signal constits with corresponding constituent jets
-      // for background mode: analyse all TrimmedVRCjets, for signal mode, pick the 2 highest pT and then analyse the one with higher mass
-
+      
+      // signal - vRC jets for further analysis with their corresponding constituent jets in SignalConstits
       PseudoJets signal; 
       vector<PseudoJets> SignalConstits;
 
@@ -361,79 +293,35 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       
       if (_mode == 0){
 
-        // input variables for the for the loop
-        double pTjet = 0;
-        double largest = 0;
-        int index = 0;
-        
-        for (int i = 0; i < VRCsize;++i){
-          pTjet = FilteredVRCjets[i].pt();
-          
-          if (pTjet >= largest){
-            largest = pTjet;
-            index = i;
-            }
-        }
+        signal = FilteredVRCjets;
+        SignalConstits = FilteredNewConstits;
 
-        signal.push_back(FilteredVRCjets[index]);
-        SignalConstits.push_back(FilteredNewConstits[index]);
       }
       
       else if (_mode  == 1) {
 
-        // input variables for the for the loop
-        double pTjet = 0;
-        double largest1 = 0;
-        double largest2 = 0;
-        int index1 = 0;
-        int index2 = 0;
-
-        for (int i = 0; i < VRCsize;++i){
-          pTjet = FilteredVRCjets[i].pt();
-          
-          if (pTjet >= largest1){
-            largest2 = largest1;
-            largest1 = pTjet;
-            index2 = index1;
-            index1 = i;
-            }
-          else if (pTjet >= largest2){
-            largest2 = pTjet;
-            index2 = i;
+        for (size_t i = 0; i < FilteredVRCjets.size(); ++i){
+          auto iterator = std::find_if(VHandtops.begin(), VHandtops.end(),
+                                  [&FilteredVRCjets, i](const Particle& VHTop){return (deltaR(momentum3(VHTop.pseudojet()), momentum3(FilteredVRCjets[i])) < 0.1);});
+          if (iterator != VHandtops.end()){ 
+            signal.push_back(FilteredVRCjets[i]);
+            SignalConstits.push_back(FilteredNewConstits[i]);
           }
-
         }
 
-        auto it_1 = std::find_if(VHandtops.begin(), VHandtops.end(),
-                                  [&FilteredVRCjets,index1](const Particle& VHTop){return (deltaR(momentum3(VHTop.pseudojet()), momentum3(FilteredVRCjets[index1])) < 0.1);});
-
-        auto it_2 = std::find_if(VHandtops.begin(), VHandtops.end(),
-                                  [&FilteredVRCjets,index2](const Particle& VHTop){return (deltaR(momentum3(VHTop.pseudojet()), momentum3(FilteredVRCjets[index2])) < 0.1);});
-
-
-        if (FilteredVRCjets[index1].m() >= FilteredVRCjets[index2].m() && it_1 != VHandtops.end()){ 
-            signal.push_back(FilteredVRCjets[index1]);
-            SignalConstits.push_back(FilteredNewConstits[index1]);
-        }
-        else if (it_2 != VHandtops.end()){
-          signal.push_back(FilteredVRCjets[index2]);
-          SignalConstits.push_back(FilteredNewConstits[index2]);
-        }
-        else {
+        if (signal.size() == 0){
           vetoEvent;
         }
 
       }
 
-      
-
-      
       // DNN tags of vRC jets
       std::vector<DNN_Category> VRCjet_tags;
       
-      // DNN scores (probabilities) -- output of the NN 
+      // DNN scores (probabilities)
       std::map<string, double> outputs;
-      
+
+      // analysis of selected vRC-jets
       for (size_t counter = 0; counter < signal.size(); ++counter){
         auto& j = signal[counter];
           
@@ -448,8 +336,7 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
           else {
             MSG_WARNING("FAILED TO FIND CONSTITUENT");
           }
-        }
-        
+        }       
 
         // Les Houches Angularity
         
@@ -465,53 +352,10 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
         
         _h["LHA"]->fill(LHA);
         
-        // number of constituent jets
-
-        size_t Nconst = SignalConstits[counter].size();
-        _h["Nconsts"]->fill(Nconst);
-
-        // N-subjettiness -- which one is correct?? the second one does not work...
-        // double beta = 1; 
-        // for (const PseudoJet& pj : SignalConstits[counter]){
-        
-        // double tau21, tau32;
-        
-        // fastjet::contrib::Nsubjettiness nSub1(1, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        // fastjet::contrib::Nsubjettiness nSub2(2, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        // fastjet::contrib::Nsubjettiness nSub3(3, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        
-        // double tau1 = nSub1.result(pj);
-        // double tau2 = nSub2.result(pj);
-        // double tau3 = nSub3.result(pj);
-        // (tau1 != 0) ? tau21 = tau2/tau1 : tau21 = -99;
-        // (tau2 != 0) ? tau32 = tau3/tau2 : tau32 = -99;
-        
-        // _h["tau21"]->fill(tau21);
-        // _h["tau32"]->fill(tau32);
-        // };
-
-        // double beta = 1;        
-        // double tau21, tau32;
-        
-        // fastjet::contrib::Nsubjettiness nSub1(1, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        // fastjet::contrib::Nsubjettiness nSub2(2, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        // fastjet::contrib::Nsubjettiness nSub3(3, fastjet::contrib::OnePass_WTA_KT_Axes(), fastjet::contrib::UnnormalizedMeasure(beta));
-        
-        // double tau1 = nSub1.result(j);
-        // double tau2 = nSub2.result(j);
-        // double tau3 = nSub3.result(j);
-        // (tau1 != 0) ? tau21 = tau2/tau1 : tau21 = -99;
-        // (tau2 != 0) ? tau32 = tau3/tau2 : tau32 = -99;
-        
-        // _h["tau21"]->fill(tau21);
-        // _h["tau32"]->fill(tau32);
-        
-
-
-        // computes the D values (probabilities)
+        //computes the D values (probabilities)
         _MCbottagger->computeScores(j, tagged_constituents, outputs);
 
-        // distriminant function P for V, H, and top tagger
+        //distriminant function P for V, H, and top tagger
         double PV=log10(outputs["dnnOutput_V"]/
         (0.9*outputs["dnnOutput_light"]+0.05*outputs["dnnOutput_top"]+0.05*outputs["dnnOutput_H"]));
         double PH=log10(outputs["dnnOutput_H"]/
@@ -524,40 +368,43 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
         _h["PH"]->fill(PH);
         _h["Pt"]->fill(Ptop);
 
-       
-       //Tag the jets in an approximation of the MCBot NN.
-
-        MCBot_TagType tag = _MCbottagger->tag(j,tagged_constituents);
-        VRCjet_tags.push_back(static_cast<DNN_Category>(tag));
-
+        //Tag the jets in an approximation of the MCBot NN.
+        MCBot_TagType DNNtag = _MCbottagger->tag(j,tagged_constituents);
+        VRCjet_tags.push_back(static_cast<DNN_Category>(DNNtag));
       }
 
-
+      
+      
       //Preselection
       //HT > 1250GeV
+      // HT = total scalar sum of the transverse momenta of all track particles and energy deposits
       double HT = std::accumulate(smeared_small_jets.begin(), smeared_small_jets.end(), 0*GeV, 
-                                  [](double count, const Jet& nextjet){return (count+nextjet.pt());});
+                                  [](double count, const Jet& nextjet){return (count+nextjet.pt());}); 
       if (HT <= 1250*GeV){
         vetoEvent;
       }
+
       //ETMiss < 200GeV and ETmiss > 40GeV
-      if (ETmiss >= 40*GeV || ETmiss <= 200*GeV){
+      if (ETmiss <= 40*GeV || ETmiss >= 200*GeV){
         vetoEvent;
       }
-      //Four small jets
+      
+      //Four small jets 
       if (smeared_small_jets.size() < 4){
         vetoEvent;
       }
-      //Minimum pT's for leading smalljets
+      
+      //Minimum pT's for leading smalljets 
       if (smeared_small_jets[0].pT() <= 300*GeV || smeared_small_jets[1].pT() <= 200*GeV ||
         smeared_small_jets[2].pT() <= 125*GeV || smeared_small_jets[3].pT() <= 75*GeV){
         vetoEvent;
       }
+    
       //2 bjets
       if (smeared_bjets.size() < 2){
         vetoEvent;
       }
-
+      
       //Two vRC jets tagged V or H
       int nVtags = std::count_if(VRCjet_tags.begin(), VRCjet_tags.end(), 
                                   [](const DNN_Category dc){return (dc == DNN_Category::V_tag);});
@@ -565,10 +412,13 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
                                   [](const DNN_Category dc){return (dc == DNN_Category::H_tag);});
       int ntoptags = std::count_if(VRCjet_tags.begin(), VRCjet_tags.end(), 
                                   [](const DNN_Category dc){return (dc == DNN_Category::top_tag);});                              
-                                  
+
       if (nVtags + nHtags < 2){
         vetoEvent;
       }
+    
+
+      
       
       
       //select signal/validation/control region.
@@ -654,7 +504,6 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
       else if (nVtags + nHtags == 2 && ntoptags >= 2 && smeared_bjets.size()==1){
         _valBins["XX_2t_1b"]->fill();
       }
-
     }
       
 
@@ -681,15 +530,12 @@ Jet JET_SMEAR_ANGULAR3(const Jet& j) {
         
         // outputs the efficiencies into a file
 
-        std::string OutputFile2 {"Eff_massive_test.csv"};
+        std::string OutputFile2 {"Efficiencies.csv"};
         std::ofstream file2;
         file2.open(OutputFile2, std::ofstream::app);
         file2 << "V-tag efficiency: " << V << ", " << "H-tag efficiency: " << H << ", " << "Top-tag efficiency: " << top << ", ";
         file2 << "\n";
         file2.close();  
-
-          
-
       }
 
       /// @}
