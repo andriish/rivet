95197223b6cb3617a6cdb1b8c0802ed14e57cd55
Peter Richardson <peter.richardson@durham.ac.uk>
2022-07-24 11:25:59 +0100

    Add BESIII_2021_I1921801 psi(2S) -> omega KS) KS0

--- analyses/pluginBES/BESIII_2012_I1088606.cc
+++ analyses/pluginBES/BESIII_2012_I1088606.cc
@@ -19,33 +19,34 @@ namespace Rivet {
 
     /// Book histograms and initialise projections before the run
     void init() {
-      // Initialise and register projections
-      UnstableParticles ufs = UnstableParticles(Cuts::pid== 443 || Cuts::pid==100443);
+      UnstableParticles ufs = UnstableParticles(Cuts::pid==443 or Cuts::pid==100443);
       declare(ufs, "UFS");
-      DecayedParticles PSI(ufs);
-      PSI.addStable(PID::PI0);
-      declare(PSI,"PSI");
-      // histos
+      DecayedParticles psi(ufs);
+      psi.addStable(PID::PI0);
+      psi.addStable(PID::K0S);
+      psi.addStable(PID::ETA);
+      psi.addStable(PID::ETAPRIME);
+      declare(psi, "psi");
       book(_h_pipi[0],1,1,1);
       book(_h_pipi[1],1,1,2);
-      book(_dalitz[0], "dalitz_Jpsi" ,50,0.,9.,50,0.0,9.);
-      book(_dalitz[1], "dalitz_psi2S",50,0.,9.,50,0.0,9.);
+      book(_dalitz[0], "dalitz_Jpsi" ,50,0.,9. ,50,0.0, 9.);
+      book(_dalitz[1], "dalitz_psi2S",50,0.,14.,50,0.0,14.);
     }
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
-      static const map<PdgId,unsigned int> & mode   = { { 211,1},{-211,1}, {111,1}};
-      DecayedParticles PSI = apply<DecayedParticles>(event, "PSI");
+      static const map<PdgId,unsigned int> & mode   = { { 211,1}, {-211,1}, { 111,1} };
+      DecayedParticles psi = apply<DecayedParticles>(event, "psi");
       // loop over particles
-      for(unsigned int ix=0;ix<PSI.decaying().size();++ix) {
-	if (!PSI.modeMatches(ix,3,mode)) continue;
-	const Particle & pip = PSI.decayProducts()[ix].at( 211)[0];
-	const Particle & pim = PSI.decayProducts()[ix].at(-211)[0];
-	const Particle & pi0 = PSI.decayProducts()[ix].at( 111)[0];
-	double mminus = (pim.momentum()+pi0.momentum()).mass2();
-	double mplus  = (pip.momentum()+pi0.momentum()).mass2();
-	double mneut  = (pip.momentum()+pim.momentum()).mass2();
-	unsigned int iloc = PSI.decaying()[ix].pid()/100000;
+      for(unsigned int ix=0;ix<psi.decaying().size();++ix) {
+	if(!psi.modeMatches(ix,3,mode)) continue;
+	const Particles & pi0 = psi.decayProducts()[ix].at( 111);
+	const Particles & pip = psi.decayProducts()[ix].at( 211);
+	const Particles & pim = psi.decayProducts()[ix].at(-211);
+	double mminus = (pim[0].momentum()+pi0[0].momentum()).mass2();
+	double mplus  = (pip[0].momentum()+pi0[0].momentum()).mass2();
+	double mneut  = (pip[0].momentum()+pim[0].momentum()).mass2();
+	unsigned int iloc = psi.decaying()[ix].pid()/100000;
 	_h_pipi[iloc]->fill(sqrt(mneut ));
 	_h_pipi[iloc]->fill(sqrt(mplus ));
 	_h_pipi[iloc]->fill(sqrt(mminus));
