d931b3c2c873c98b8b9168c46550733a80a56501
Peter Richardson <peter.richardson@durham.ac.uk>
2022-07-25 20:48:38 +0100

    Add BESIII_2013_I1223625 J/psi -> p pbar omega

--- analyses/pluginBES/BESIII_2013_I1223625.cc
+++ analyses/pluginBES/BESIII_2013_I1223625.cc
@@ -6,12 +6,12 @@
 namespace Rivet {
 
 
-  /// @brief psi(2S) -> p pbar pi0
-  class BESIII_2013_I1120737 : public Analysis {
+  /// @brief J/psi -> p pbar omega
+  class BESIII_2013_I1223625 : public Analysis {
   public:
 
     /// Constructor
-    RIVET_DEFAULT_ANALYSIS_CTOR(BESIII_2013_I1120737);
+    RIVET_DEFAULT_ANALYSIS_CTOR(BESIII_2013_I1223625);
 
 
     /// @name Analysis methods
@@ -19,32 +19,37 @@ namespace Rivet {
 
     /// Book histograms and initialise projections before the run
     void init() {
-      UnstableParticles ufs = UnstableParticles(Cuts::pid==100443);
+      UnstableParticles ufs = UnstableParticles(Cuts::pid==443);
       declare(ufs, "UFS");
       DecayedParticles psi(ufs);
       psi.addStable(PID::PI0);
       psi.addStable(PID::K0S);
       psi.addStable(PID::ETA);
       psi.addStable(PID::ETAPRIME);
+      psi.addStable(PID::OMEGA);
       declare(psi, "psi");
-      book(_h_ppi0,1,1,1);
-      book(_dalitz, "dalitz",50,1.,8.,50,1.,8.);
+      for(unsigned int ix=0;ix<3;++ix)
+	book(_h[ix],1,1,1+ix);
+      book(_dalitz, "dalitz",50,2.5,5.,50,2.5,5.);
     }
 
 
     /// Perform the per-event analysis
     void analyze(const Event& event) {
-      static const map<PdgId,unsigned int> & mode   = { { 2212,1}, {-2212,1}, { 111,1} };
+      static const map<PdgId,unsigned int> & mode   = { { 2212,1}, {-2212,1}, { 223,1} };
       DecayedParticles psi = apply<DecayedParticles>(event, "psi");
       // loop over particles
       for(unsigned int ix=0;ix<psi.decaying().size();++ix) {
 	if(!psi.modeMatches(ix,3,mode)) continue;
-	const Particles & pi0  = psi.decayProducts()[ix].at( 111);
-	const Particles & pp   = psi.decayProducts()[ix].at( 2212);
-	const Particles & pbar = psi.decayProducts()[ix].at(-2212);
-	double mminus = (pbar[0].momentum()+pi0 [0].momentum()).mass2();
-	double mplus  = (pp  [0].momentum()+pi0 [0].momentum()).mass2();
-	_h_ppi0->fill(sqrt(mplus ));
+	const Particles & omega = psi.decayProducts()[ix].at( 223);
+	const Particles & pp    = psi.decayProducts()[ix].at( 2212);
+	const Particles & pbar  = psi.decayProducts()[ix].at(-2212);
+	double mminus = (pbar[0].momentum()+omega[0].momentum()).mass2();
+	double mplus  = (pp  [0].momentum()+omega[0].momentum()).mass2();
+	double mneut  = (pp  [0].momentum()+pbar [0].momentum()).mass2();
+	_h[0]->fill(sqrt(mplus ));
+	_h[1]->fill(sqrt(mneut ));
+	_h[2]->fill(sqrt(mminus));
 	_dalitz->fill(mplus,mminus);
       }
     }
@@ -52,8 +57,9 @@ namespace Rivet {
 
     /// Normalise histograms etc., after the run
     void finalize() {
+      for(unsigned int ix=0;ix<3;++ix)
+	normalize(_h[ix]);
       normalize(_dalitz);
-      normalize(_h_ppi0);
     }
 
     /// @}
@@ -61,7 +67,7 @@ namespace Rivet {
 
     /// @name Histograms
     /// @{
-    Histo1DPtr _h_ppi0;
+    Histo1DPtr _h[3];
     Histo2DPtr _dalitz;
     /// @}
 
@@ -69,6 +75,6 @@ namespace Rivet {
   };
 
 
-  RIVET_DECLARE_PLUGIN(BESIII_2013_I1120737);
+  RIVET_DECLARE_PLUGIN(BESIII_2013_I1223625);
 
 }
