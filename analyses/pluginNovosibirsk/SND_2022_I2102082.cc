3819308a0342d60658b4c5681f1816203b162017
Peter Richardson <peter.richardson@durham.ac.uk>
2022-09-08 13:05:24 +0100

    Add SND_2022_I2102082 e+e- > n nbar

--- analyses/pluginNovosibirsk/SND_2022_I2102082.cc
+++ analyses/pluginNovosibirsk/SND_2022_I2102082.cc
@@ -2,20 +2,19 @@
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
 
-
 namespace Rivet {
 
 
   /// @brief e+ e- > n nbar
-  class SND_2014_I1321689 : public Analysis {
+  class SND_2022_I2102082 : public Analysis {
   public:
 
     /// Constructor
-    RIVET_DEFAULT_ANALYSIS_CTOR(SND_2014_I1321689);
+    RIVET_DEFAULT_ANALYSIS_CTOR(SND_2022_I2102082);
 
 
     /// @name Analysis methods
-    //@{
+    /// @{
 
     /// Book histograms and initialise projections before the run
     void init() {
@@ -42,46 +41,40 @@ namespace Rivet {
 
     /// Normalise histograms etc., after the run
     void finalize() {
-
       double sigma = _nneutron->val();
       double error = _nneutron->err();
       sigma *= crossSection()/ sumOfWeights() /nanobarn;
       error *= crossSection()/ sumOfWeights() /nanobarn;
-      for(unsigned int ix=1;ix<3;++ix) {
-	Scatter2D temphisto(refData(ix, 1, 1));
-	Scatter2DPtr mult;
-	book(mult, ix, 1, 1);
-	for (size_t b = 0; b < temphisto.numPoints(); b++) {
-	  const double x  = temphisto.point(b).x();
-	  pair<double,double> ex = temphisto.point(b).xErrs();
-	  pair<double,double> ex2 = ex;
-	  if(ex2.first ==0.) ex2. first=0.0001;
-	  if(ex2.second==0.) ex2.second=0.0001;
-	  if (inRange(sqrtS()/MeV, x-ex2.first, x+ex2.second)) {
-	    mult->addPoint(x, sigma, ex, make_pair(error,error));
-	  }
-	  else {
-	    mult->addPoint(x, 0., ex, make_pair(0.,.0));
-	  }
+      Scatter2D temphisto(refData(1, 1, 1));
+      Scatter2DPtr mult;
+      book(mult, 1, 1, 1);
+      for (size_t b = 0; b < temphisto.numPoints(); b++) {
+	const double x  = temphisto.point(b).x();
+	pair<double,double> ex = temphisto.point(b).xErrs();
+	pair<double,double> ex2 = ex;
+	if(ex2.first ==0.) ex2. first=0.0001;
+	if(ex2.second==0.) ex2.second=0.0001;
+	if (inRange(sqrtS()/MeV, x-ex2.first, x+ex2.second)) {
+	  mult->addPoint(x, sigma, ex, make_pair(error,error));
+	}
+	else {
+	  mult->addPoint(x, 0., ex, make_pair(0.,.0));
 	}
       }
-
     }
 
-    //@}
+    /// @}
 
 
     /// @name Histograms
-    //@{
+    /// @{
     CounterPtr _nneutron;
-    //@}
+    /// @}
 
 
   };
 
 
-  // The hook for the plugin system
-  RIVET_DECLARE_PLUGIN(SND_2014_I1321689);
-
+  RIVET_DECLARE_PLUGIN(SND_2022_I2102082);
 
 }
