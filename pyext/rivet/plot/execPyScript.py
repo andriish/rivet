c87bad7dba9c7f98d2fd0690b71c4bcefedf7361
yoranyeh <yoranyeh@gmail.com>
2022-11-04 14:38:00 +0100

    main safeguard for multiprocessing pool

--- pyext/rivet/plot/execPyScript.py
+++ pyext/rivet/plot/execPyScript.py
@@ -1,9 +1,16 @@
 import os
+from multiprocessing import Pool
 
 def execute(pyScript):
   if not os.path.isfile(pyScript):
-    raise IOError("Python script not found!")
+    raise FileNotFoundError("Python script not found!")
 
   # supply globals() argument to add imports to global scope
-  return exec(open(pyScript).read(), globals())
+  exec(open(pyScript).read(), globals())
+  
+
+def executePyScripts(pyScripts): 
+  from multiprocessing import Pool
+  p = Pool(processes=4)
+  p.map(execute, pyScripts)
   
