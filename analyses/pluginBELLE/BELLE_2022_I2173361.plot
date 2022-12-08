9e1a9683ff03146403c865417aa8a9e28fb36574
Peter Richardson <peter.richardson@durham.ac.uk>
2022-12-08 11:45:44 +0000

    Add BESIII_2022_I2611486 e+ e- -> Omega- Omegabar+

--- analyses/pluginBELLE/BELLE_2022_I2173361.plot
+++ analyses/pluginBELLE/BELLE_2022_I2173361.plot
@@ -1,7 +1,7 @@
 BEGIN PLOT /BELLE_2022_I2173361/d01-x01-y01
 Title=Cross section for $e^+e^-\to\Sigma^0\bar{\Sigma}^0$
 XLabel=$\sqrt{s}$ [GeV]
-YLabel=$\sigma(e^+e^-\to\Sigma^+\bar{\Sigma}^-)$ [pb]
+YLabel=$\sigma(e^+e^-\to\Sigma^0\bar{\Sigma}^0)$ [pb]
 ConnectGaps=1
 LogY=0
 END PLOT
---
+++ analyses/pluginBES/BESIII_2022_I2611486.cc
@@ -0,0 +1,118 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/UnstableParticles.hh"
+
+namespace Rivet {
+
+
+  /// @brief e+ e- > Omega- Omegabar+
+  class BESIII_2022_I2611486 : public Analysis {
+  public:
+
+    /// Constructor
+    RIVET_DEFAULT_ANALYSIS_CTOR(BESIII_2022_I2611486);
+
+
+    /// @name Analysis methods
+    /// @{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+      // projections
+      declare(FinalState(), "FS");
+      declare(UnstableParticles(Cuts::abspid==3334), "UFS");
+      // histos
+      book(_c,"TMP/c");
+    }
+
+    void findChildren(const Particle & p,map<long,int> & nRes, int &ncount) {
+      for(const Particle &child : p.children()) {
+	if(child.children().empty()) {
+	  nRes[child.pid()]-=1;
+	  --ncount;
+	}
+	else
+	  findChildren(child,nRes,ncount);
+      }
+    }
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+      const FinalState& fs = apply<FinalState>(event, "FS");
+      // total hadronic and muonic cross sections
+      map<long,int> nCount;
+      int ntotal(0);
+      for (const Particle& p : fs.particles()) {
+      	nCount[p.pid()] += 1;
+      	++ntotal;
+      }
+      // find the Omegas
+      const FinalState& ufs = apply<UnstableParticles>(event, "UFS");
+      for(const Particle& p1 : ufs.particles()) {
+	bool matched = false;
+	// find the children
+	map<long,int> nRes = nCount;
+	int ncount = ntotal;
+	findChildren(p1,nRes,ncount);
+	for(const Particle& p2 : ufs.particles()) {
+	  if(p2.pid() != -p1.pid()) continue;
+	  map<long,int> nRes2 = nRes;
+	  int ncount2 = ncount;
+	  findChildren(p2,nRes2,ncount2);
+	  if(ncount2!=0) continue;
+	  matched=true;
+	  for(auto const & val : nRes2) {
+	    if(val.second!=0) {
+	      matched = false;
+	      break;
+	    }
+	  }
+	  if(matched) {
+	    _c->fill();
+	    break;
+	  }
+	}
+	if(matched) break;
+      }
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+      double fact = crossSection()/ sumOfWeights() /femtobarn;
+      double sigma = _c->val()*fact;
+      double error = _c->err()*fact;
+      Scatter2D temphisto(refData(1, 1, 1));
+      Scatter2DPtr  mult;
+      book(mult, 1, 1, 1);
+      for (size_t b = 0; b < temphisto.numPoints(); b++) {
+	const double x  = temphisto.point(b).x();
+	pair<double,double> ex = temphisto.point(b).xErrs();
+	pair<double,double> ex2 = ex;
+	if(ex2.first ==0.) ex2. first=0.0001;
+	if(ex2.second==0.) ex2.second=0.0001;
+	if (inRange(sqrtS()/GeV, x-ex2.first, x+ex2.second)) {
+	    mult->addPoint(x, sigma, ex, make_pair(error,error));
+	}
+	else {
+	  mult->addPoint(x, 0., ex, make_pair(0.,.0));
+	}
+      }
+    }
+    
+    /// @}
+
+
+    /// @name Histograms
+    /// @{
+    CounterPtr _c;
+    /// @}
+
+
+  };
+
+
+  RIVET_DECLARE_PLUGIN(BESIII_2022_I2611486);
+
+}
