43e581583603e36d8bc896ee9f8761196e2d6a47
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-02-09 16:54:40 +0100

    Try to run Github actions on Mac

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -7,7 +7,7 @@ TOP=$(pwd)
 
 autoreconf --force --install --verbose .
 automake -a --force
-configure  --prefix=$TOP/INSTALL --disable-doxygen --with-yoda=$(yoda-config --prefix ) --with-hepmc3=$(HepMC3-config --prefix) --with-fjcontrib=$(fastjet-config --prefix) --with-fastjet=$(fastjet-config --prefix)
+./configure  --prefix=$TOP/INSTALL --disable-doxygen --with-yoda=$(yoda-config --prefix ) --with-hepmc3=$(HepMC3-config --prefix) --with-fjcontrib=$(fastjet-config --prefix) --with-fastjet=$(fastjet-config --prefix)
 make -j 4
 make install
 find $TOP/INSTALL
