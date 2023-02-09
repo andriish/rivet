7cdc9a1a10acf5fc1eabecb9909e655333817299
Andrii Verbytskyi <andriish@pcatlas18.mpp.mpg.de>
2023-02-09 17:23:55 +0100

    Try to run Github actions on Mac

--- .github/workflows/osx/entrypoint.sh
+++ .github/workflows/osx/entrypoint.sh
@@ -2,9 +2,19 @@
 set -x
 brew install wget coreutils gcc autoconf automake cython
 brew tap davidchall/hep
-brew install hepmc3 yoda fastjet fjcontrib 
+brew install hepmc3 yoda fastjet 
 TOP=$(pwd)
 
+mkdir FJ
+cd FJ
+wget https://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.048.tar.gz
+tar zxfv fjcontrib-1.048.tar.gz
+./configure --prefix=$(fastjet-config --prefix)
+make fragile-shared-install
+make install
+
+
+cd $TOP
 autoreconf --force --install --verbose .
 automake -a --force
 ./configure  --prefix=$TOP/INSTALL --disable-doxygen --with-yoda=$(yoda-config --prefix ) --with-hepmc3=$(HepMC3-config --prefix) --with-fjcontrib=$(fastjet-config --prefix) --with-fastjet=$(fastjet-config --prefix)
